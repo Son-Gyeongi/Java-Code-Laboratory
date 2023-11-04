010d31e4a725b71bf5a66c02714d283ad71c1409
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-04 18:41:21 +0900

    File save Practice

---
+++ src/main/java/com/ll/date20231104/FileTest.java
@@ -0,0 +1,21 @@
+package com.ll.date20231104;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileTest {
+    public static void main(String[] args) {
+        String fileName = "example.txt";
+        String content = "파일에 저장될 내용입니다.";
+
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            writer.write(content);
+            writer.close();
+            System.out.println("파일이 성공적으로 저장되었습니다.");
+        } catch (IOException e) {
+            System.out.println("파일 저장 중 오류가 발생했습니다.");
+            e.printStackTrace();
+        }
+    }
+}
