b1e96292bb73266d7ce8c52367dd520ca26aa347
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-07 02:56:24 +0900

    약수 구하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(1234));
+        System.out.println(s.solution(5));
     }
 }
 
@@ -13,9 +13,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        while (n > 0) {
-            answer += (n % 10);
-            n = n / 10;
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > 0; j--) {
+                if (i * j == n) {
+                    answer += i;
+                }
+            }
         }
 
         return answer;
