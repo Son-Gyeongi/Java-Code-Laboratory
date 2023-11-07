af75c8a976330a9a7e64e527674a03a6e80f2490
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-07 10:08:42 +0900

    삼각형의 완성조건(1)

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,22 +5,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(5));
+        int[] arr = {199, 72, 222};
+        System.out.println(s.solution(arr));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
+    public int solution(int[] sides) {
+        // 배열 정렬
+        int[] sidesSort = Arrays.stream(sides).sorted().toArray();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j > 0; j--) {
-                if (i * j == n) {
-                    answer += i;
-                }
-            }
-        }
-
-        return answer;
+        return sidesSort[2] < sidesSort[1] + sidesSort[0] ? 1 : 2;
     }
 }
