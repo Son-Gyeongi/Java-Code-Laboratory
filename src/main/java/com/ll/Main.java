29548778f79774735bdc13ca147c7c6fbf3875f2
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-10 11:11:31 +0900

    제곱 수 판별 - 정확성 오류 참고하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,28 +1,33 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(345000));
+        System.out.println(s.solution(999997));
     }
 }
 
 class Solution {
-    public int solution(int price) {
-
-        // 10만 원 이상 사면 5% 할인
-        // 30만 원 이상 사면 10% 할인
-        // 50만 원 이상 사면 20% 할인
-        if (price >= 500000) {
-            price = (int) (price * 0.8);
-        } else if (price >= 300000) {
-            price = (int) (price * 0.9);
-        } else if (price >= 100000) {
-            price = (int) (price * 0.95);
-        }
-
-        return price;
+    public int solution(int n) {
+        // 제곱근을 정수로 변환하여 계산
+        System.out.println(Math.sqrt(n));
+        System.out.println(Math.pow(Math.sqrt(n), 2));
+        System.out.println(n == Math.pow(Math.sqrt(n), 2));
+        System.out.println((int) Math.sqrt(n));
+        int sqrt = (int) Math.sqrt(n);
+        System.out.println(Math.pow(sqrt, 2));
+        return n == sqrt * sqrt ? 1 : 2;
     }
 }
+
+//class Solution {
+//    public double solution(int n) {
+//        // 제곱근 메서드를 거듭제곱 메서드 안에 넣기
+//        System.out.println(Math.sqrt(n));
+//        System.out.println(Math.pow(Math.sqrt(n), 2));
+//        System.out.println(n == Math.pow(Math.sqrt(n), 2));
+//        double sqrt = Math.sqrt(n);
+//        System.out.println(sqrt*sqrt);
+//        return sqrt*sqrt == Math.pow(Math.sqrt(n), 2) ? 1 : 2;
+//    }
+//}
