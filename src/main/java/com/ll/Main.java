68a4f73431c1c8ba520a271075c68d09f6d6e15c
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-10 11:24:34 +0900

    문자 반복 출력하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,31 +3,20 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(999997));
+        System.out.println(s.solution("hello",3));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        // 제곱근을 정수로 변환하여 계산
-        System.out.println(Math.sqrt(n));
-        System.out.println(Math.pow(Math.sqrt(n), 2));
-        System.out.println(n == Math.pow(Math.sqrt(n), 2));
-        System.out.println((int) Math.sqrt(n));
-        int sqrt = (int) Math.sqrt(n);
-        System.out.println(Math.pow(sqrt, 2));
-        return n == sqrt * sqrt ? 1 : 2;
+    public String solution(String my_string, int n) {
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            for (int j = 1; j <= n; j++) {
+                answer += my_string.charAt(i);
+            }
+        }
+
+        return answer;
     }
 }
-
-//class Solution {
-//    public double solution(int n) {
-//        // 제곱근 메서드를 거듭제곱 메서드 안에 넣기
-//        System.out.println(Math.sqrt(n));
-//        System.out.println(Math.pow(Math.sqrt(n), 2));
-//        System.out.println(n == Math.pow(Math.sqrt(n), 2));
-//        double sqrt = Math.sqrt(n);
-//        System.out.println(sqrt*sqrt);
-//        return sqrt*sqrt == Math.pow(Math.sqrt(n), 2) ? 1 : 2;
-//    }
-//}
