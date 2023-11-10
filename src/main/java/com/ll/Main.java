784caee9b7caae17a61d646f43b7a4b9d698e517
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-10 14:59:50 +0900

    [프로그래머스] 모음 제거

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,20 +3,25 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("hello",3));
+        System.out.println(s.solution("hello"));
     }
 }
 
 class Solution {
-    public String solution(String my_string, int n) {
-        String answer = "";
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < my_string.length(); i++) {
-            for (int j = 1; j <= n; j++) {
-                answer += my_string.charAt(i);
+            if (my_string.charAt(i) == 'a'
+                    | my_string.charAt(i) == 'e'
+                    | my_string.charAt(i) == 'i'
+                    | my_string.charAt(i) == 'o'
+                    | my_string.charAt(i) == 'u') {
+                continue;
             }
+            answer.append(my_string.charAt(i));
         }
 
-        return answer;
+        return answer.toString();
     }
 }
