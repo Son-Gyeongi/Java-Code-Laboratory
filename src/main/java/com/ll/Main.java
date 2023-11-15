e56a01fecaee0fcaf2231d5cb1af175c8a217510
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-15 16:09:47 +0900

    조건에 맞게 수열 변환하기 1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,27 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("abcdevwxyz"));
+        int[] arr = {1, 2, 3, 100, 99, 98};
+        System.out.println(Arrays.toString(s.solution(arr)));
     }
 }
 
 class Solution {
-    public String solution(String myString) {
-        return myString.replaceAll("[a-k]", "l");
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 50 && arr[i] % 2 == 0) {
+                answer[i] = arr[i] / 2;
+            } else if (arr[i] < 50 && arr[i] % 2 == 1) {
+                answer[i] = arr[i] * 2;
+            } else answer[i] = arr[i];
+        }
+
+        return answer;
     }
 }
