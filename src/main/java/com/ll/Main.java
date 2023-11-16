8e884c34035b1186c3d0d8a2a7503783b1bbf49c
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-16 11:39:41 +0900

    부분 문자열

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(55, 2,5));
+        System.out.println(s.solution("m", "aabcc"));
     }
 }
 
 class Solution {
-    public int solution(int number, int n, int m) {
-        return number % n == 0 && number % m == 0 ? 1 : 0;
+    public int solution(String str1, String str2) {
+        return str2.contains(str1) ? 1 : 0;
     }
 }
