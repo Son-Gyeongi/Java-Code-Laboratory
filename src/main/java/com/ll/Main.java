7e025dcf1814eaeeb02a3153960867e5b0635e73
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-17 15:08:51 +0900

    문자열의 앞의 n글자

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("masterpiece"));
+        System.out.println(s.solution("ProgrammerS123", 11));
     }
 }
 
 class Solution {
-    public String solution(String rny_string) {
-        return rny_string.replace("m", "rn");
+    public String solution(String my_string, int n) {
+        // 문자열의 시작은 0부터 ~ n-1까지 출력이 된다.
+        return my_string.substring(0, n);
     }
 }
