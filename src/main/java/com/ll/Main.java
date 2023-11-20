26934428a36bd527426cd627bed9f8474d0ca141
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-20 11:06:58 +0900

    길이에 따른 연산

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,12 +6,27 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("aBcDeFg"));
+        int[] arr = {2, 3, 4, 5};
+        System.out.println(s.solution(arr));
     }
 }
 
 class Solution {
-    public String solution(String myString) {
-        return myString.toLowerCase();
+    public int solution(int[] num_list) {
+        int answer = 0;
+
+        // 배열 길이 11이상이면 더하기, 배열 길이 10이하이면 곱하기
+        if (num_list.length >= 11) {
+            for (int num : num_list) {
+                answer += num;
+            }
+        } else {
+            answer =1;
+            for (int num : num_list) {
+                answer *= num;
+            }
+        }
+
+        return answer;
     }
 }
