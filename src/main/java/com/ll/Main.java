525fdb3d453c511a0e521ab032b2081ac35c50db
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-21 19:08:10 +0900

    정수 부분

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,26 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr = {2, 3, 4, 5};
-        System.out.println(s.solution(arr));
+        System.out.println(s.solution(69.42));
     }
 }
 
 class Solution {
-    public int solution(int[] num_list) {
-        int answer = 0;
-
-        // 배열 길이 11이상이면 더하기, 배열 길이 10이하이면 곱하기
-        if (num_list.length >= 11) {
-            for (int num : num_list) {
-                answer += num;
-            }
-        } else {
-            answer =1;
-            for (int num : num_list) {
-                answer *= num;
-            }
-        }
-
-        return answer;
+    public int solution(double flo) {
+        return (int) flo;
     }
 }
