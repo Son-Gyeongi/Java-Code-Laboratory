e1f56329f098cedcac6a8bd8799732cba484ada9
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-21 19:36:02 +0900

    주사위 게임

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,12 +7,17 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr = {2, 3, 4, 5};
-        System.out.println(Arrays.toString(s.solution("i love you")));
+        System.out.println(s.solution(2,4));
     }
 }
 
 class Solution {
-    public String[] solution(String my_string) {
-        return my_string.split(" ");
+    public int solution(int a, int b) {
+        boolean _a = (a % 2 == 1); // 홀수 true, 짝수 false
+        boolean _b = (b % 2 == 1);
+
+        if (_a && _b) return a*a + b*b;
+        else if (_a || _b) return 2 * (a + b);
+        else return Math.abs(a - b);
     }
 }
