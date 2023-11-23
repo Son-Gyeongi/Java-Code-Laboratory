47dd21cf173a7f957940fd62b6f3e41befb1e187
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-23 13:33:22 +0900

    첫 번째로 나오는 음수

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,23 +1,24 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {2, 3, 4, 5};
-        System.out.println(s.solution(2,4));
+        int[] arr = {12, 4, 15, 46, 38, -2, 15};
+        System.out.println(s.solution(arr));
     }
 }
 
 class Solution {
-    public int solution(int a, int b) {
-        boolean _a = (a % 2 == 1); // 홀수 true, 짝수 false
-        boolean _b = (b % 2 == 1);
+    public int solution(int[] num_list) {
 
-        if (_a && _b) return a*a + b*b;
-        else if (_a || _b) return 2 * (a + b);
-        else return Math.abs(a - b);
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
