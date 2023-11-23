f260adcc917425f1c16fa5ed6a88aedd34c30891
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-23 16:06:08 +0900

    배열 비교하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,24 +1,31 @@
 package com.ll;
 
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {12, 4, 15, 46, 38, -2, 15};
-        System.out.println(s.solution(arr));
+        int[] arr1 = {1, 2, 3, 4,5};
+        int[] arr2 = {3,3,3,3,3};
+        System.out.println(s.solution(arr1, arr2));
     }
 }
 
 class Solution {
-    public int solution(int[] num_list) {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int arr1Sum =0;
+        int arr2Sum =0;
 
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] < 0) {
-                return i;
-            }
+        if (arr1.length > arr2.length) return 1;
+        else if (arr1.length < arr2.length) return -1;
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr1Sum += arr1[i];
+            arr2Sum += arr2[i];
         }
 
-        return -1;
+        // arr1Sum > arr2Sum ? 1 : arr1Sum < arr2Sum ? -1 : 0
+        return Integer.compare(arr1Sum, arr2Sum);
     }
 }
