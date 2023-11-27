1c9c67b8243382748f2ffbbf1659114cd8e2fe92
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-27 22:04:12 +0900

    카운트 다운

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,23 +1,22 @@
 package com.ll;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr1 = {1, 2, 3, 4,5};
-        System.out.println(Arrays.toString(s.solution(3, 10)));
+        System.out.println(Arrays.toString(s.solution(10, 3)));
     }
 }
 
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num + 1];
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start - end_num + 1];
 
-        int j = 0;
-        for (int i = start_num; i <= end_num; i++) {
-            answer[j] = i;
-            ++j;
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start;
+            start = start - 1;
         }
 
         return answer;
