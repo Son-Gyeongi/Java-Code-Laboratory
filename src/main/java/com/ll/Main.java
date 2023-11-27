ec2587720c17b0960f6f242e590f0361a17851f7
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-27 21:31:16 +0900

    카운트 업

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,15 +1,25 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr1 = {1, 2, 3, 4,5};
-        System.out.println(s.solution(-4, -7, true));
+        System.out.println(Arrays.toString(s.solution(3, 10)));
     }
 }
 
 class Solution {
-    public int solution(int a, int b, boolean flag) {
-        return flag ? a + b : a - b;
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+
+        int j = 0;
+        for (int i = start_num; i <= end_num; i++) {
+            answer[j] = i;
+            ++j;
+        }
+
+        return answer;
     }
 }
