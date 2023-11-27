5393c5a7299896f836d79ad2e1e1c67a9a70758a
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-27 10:26:13 +0900

    flag에 따라 다른 값 반환하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,28 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] arr1 = {1, 2, 3, 4,5};
-        System.out.println(Arrays.toString(s.solution(arr1, 27)));
+        System.out.println(s.solution(-4, -7, true));
     }
 }
 
 class Solution {
-    public int[] solution(int[] arr, int n) {
-        int[] answer = new int[arr.length];
-
-        if (arr.length % 2 == 0) {
-            for (int i = 0; i < arr.length; i++) {
-                if (i % 2 == 1) {
-                    answer[i] = arr[i] + n;
-                } else answer[i] = arr[i];
-            }
-        } else {
-            for (int i = 0; i < arr.length; i++) {
-                if (i % 2 == 0) {
-                    answer[i] = arr[i] + n;
-                } else answer[i] = arr[i];
-            }
-        }
-
-        return answer;
+    public int solution(int a, int b, boolean flag) {
+        return flag ? a + b : a - b;
     }
 }
