402af2b515a73d2ba5f0562bb4f9948c0f46a0e0
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-29 22:01:32 +0900

    홀짝에 따라 다른 값 반환하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,16 +5,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("string", 3));
+        System.out.println(s.solution(7));
     }
 }
 
 class Solution {
-    public String solution(String my_string, int k) {
-        String answer = "";
+    public int solution(int n) {
+        int answer = 0;
 
-        for (int i = 0; i < k; i++) {
-            answer += my_string;
+        // n이 홀수면 홀수 숫자만, 짝수면 짝수 숫자만
+        // 거르고 나서 반복해서 로직 수행
+
+        for (int i = (n % 2 == 0) ? 0 : 1; i <= n; i=i+2) {
+            if (n % 2 == 1) { // 홀수
+                answer += i;
+            } else {
+                answer += (int) Math.pow(i, 2);
+            }
         }
 
         return answer;
