b2887fc6c7417131e8caeb6242554ce5d8989671
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-29 12:06:30 +0900

    이어 붙인 수

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,24 +1,28 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(10, 3)));
+        int[] arr = {3, 4, 5, 2, 1};
+        System.out.println(s.solution(arr));
     }
 }
 
 class Solution {
-    public int[] solution(int start, int end_num) {
-        int[] answer = new int[start - end_num + 1];
+    public int solution(int[] num_list) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
 
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = start;
-            start = start - 1;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) sb1.append(num_list[i]);
+            else sb2.append(num_list[i]);
         }
 
-        return answer;
+        int a = Integer.parseInt(String.valueOf(sb1));
+        int b = Integer.parseInt(String.valueOf(sb2));
+
+        return a + b;
     }
 }
