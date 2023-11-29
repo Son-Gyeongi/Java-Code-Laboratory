ee7a6b5fa7421d7518ce9148e3f7cc43bfe60c9b
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-29 12:27:22 +0900

    문자열 곱하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,24 +5,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {3, 4, 5, 2, 1};
-        System.out.println(s.solution(arr));
+        System.out.println(s.solution("string", 3));
     }
 }
 
 class Solution {
-    public int solution(int[] num_list) {
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+    public String solution(String my_string, int k) {
+        String answer = "";
 
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] % 2 == 0) sb1.append(num_list[i]);
-            else sb2.append(num_list[i]);
+        for (int i = 0; i < k; i++) {
+            answer += my_string;
         }
 
-        int a = Integer.parseInt(String.valueOf(sb1));
-        int b = Integer.parseInt(String.valueOf(sb2));
-
-        return a + b;
+        return answer;
     }
 }
