fb855716aeef4d90ad0fd4303c4dbf246e5211dc
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-07 08:43:10 +0900

    홀짝 구분하기

--- src/main/java/com/ll/Solution1.java
+++ src/main/java/com/ll/Solution1.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        String b = sc.next();
+        int n = sc.nextInt();
 
-        System.out.println(a+b);
+        if (n%2==0) System.out.println(n + " is even");
+        else System.out.println(n + " is odd");
     }
 }
