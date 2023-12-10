451326e057d99cf0f2ff0d4c5251c4016ba27da3
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-10 22:38:33 +0900

    마지막 두 원소

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,12 +6,20 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(123));
+        int[] arr = {5, 2, 1, 7, 5};
+        System.out.println(Arrays.toString(s.solution(arr)));
     }
 }
 
 class Solution {
-    public String solution(int n) {
-        return String.valueOf(n);
+    public int[] solution(int[] num_list) {
+        int numLastBefore = num_list[num_list.length - 2];
+        int numLast = num_list[num_list.length - 1];
+
+        int[] answer = Arrays.copyOfRange(num_list, 0, num_list.length+1);
+        System.out.println(Arrays.toString(answer));
+        answer[answer.length - 1] = numLast > numLastBefore ? numLast - numLastBefore : numLast * 2;
+
+        return answer;
     }
 }
