94e1cc8b3e7a9e4193a84d01015e4d75016dcefc
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-11 13:38:37 +0900

    문자열 바꿔서 찾기 - replace()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,25 +1,21 @@
 package com.ll;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {5, 2, 1, 7, 5};
-        System.out.println(Arrays.toString(s.solution(arr)));
+        System.out.println(s.solution("ABBAA", "ABAB"));
     }
 }
 
 class Solution {
-    public int[] solution(int[] num_list) {
-        int numLastBefore = num_list[num_list.length - 2];
-        int numLast = num_list[num_list.length - 1];
+    public int solution(String myString, String pat) {
+        String strArr = "";
 
-        int[] answer = Arrays.copyOfRange(num_list, 0, num_list.length+1);
-        System.out.println(Arrays.toString(answer));
-        answer[answer.length - 1] = numLast > numLastBefore ? numLast - numLastBefore : numLast * 2;
-
-        return answer;
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'A') strArr += 'B';
+            if (myString.charAt(i) == 'B') strArr += 'A';
+        }
+        System.out.println(strArr);
+        return strArr.contains(pat) ? 1 : 0;
     }
 }
