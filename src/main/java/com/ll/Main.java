b50951e02b5ea98de84a62154cee22a2599a6064
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-16 00:36:40 +0900

    꼬리 문자열 - Arrays.stream().filter().reduce()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,21 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("banana", "ana"));
+        String[] str_list = {"abc", "def", "ghi"};
+        System.out.println(s.solution(str_list, "ef"));
+        // 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열
     }
 }
 
 class Solution {
-    public int solution(String my_string, String target) {
-        return my_string.contains(target) ? 1 : 0;
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list)
+                .filter(str -> !str.contains(ex)) // ex가 포함되지 않는 문자열을 찾는다.
+                .reduce("", (acc, str) -> acc + str);
+        // "" : 초기값, acc : 누적값, str : filter에서 넘어오는 각 요소
     }
 }
