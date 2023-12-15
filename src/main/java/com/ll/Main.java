9ef3491a9f6b86169536679190b48acdf0833052
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-16 00:18:50 +0900

    부분 문자열인지 확인하기 - contains()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,12 +3,12 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("ProgrammerS123", 11));
+        System.out.println(s.solution("banana", "ana"));
     }
 }
 
 class Solution {
-    public String solution(String my_string, int n) {
-        return my_string.substring(my_string.length() - n);
+    public int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1 : 0;
     }
 }
