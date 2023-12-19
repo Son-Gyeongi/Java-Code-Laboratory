1ffadc04f63b46d5d1b0705298bd13e0dfbc5020
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-20 00:15:15 +0900

    n의 배수 고르기 - Arrays.stream(numlist).filter(num -> num % n == 0).toArray()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] str_list = {"abc", "def", "ghi"};
-        System.out.println(s.solution("aBcDeFg"));
+        int[] arr = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.println(Arrays.toString(s.solution(3, arr)));
         // 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열
     }
 }
 
 class Solution {
-    public String solution(String myString) {
-        return myString.toUpperCase();
+    public int[] solution(int n, int[] numlist) {
+        return Arrays.stream(numlist).filter(num -> num % n == 0).toArray();
     }
 }
