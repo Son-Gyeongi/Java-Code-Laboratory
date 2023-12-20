f408f14a129d772306127cf9306b51cc0f16a391
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-20 13:28:18 +0900

    수 조작하기 1 - case문

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,23 +1,28 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {5, 2, 1, 7, 5};
-        System.out.println(Arrays.toString(s.solution(arr, 2)));
+        System.out.println(s.solution(0, "wsdawsdassw"));
         // 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열
     }
 }
 
 class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length-(n-1)];
+    public int solution(int n, String control) {
+        int answer = n;
 
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[n-1];
-            n++;
+        /*
+        "w" : n이 1 커집니다.
+        "s" : n이 1 작아집니다.
+        "d" : n이 10 커집니다.
+        "a" : n이 10 작아집니다.
+         */
+        for (int i = 0; i < control.length(); i++) {
+            if (control.charAt(i) == 'w') answer += 1;
+            else if (control.charAt(i) == 's') answer -= 1;
+            else if (control.charAt(i) == 'd') answer += 10;
+            else answer -= 10;
         }
 
         return answer;
