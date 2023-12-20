a20de02e9a5d5ac1a42d18cda2c7b5f7e9b6899d
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-20 09:37:45 +0900

    n 번째 원소부터 - Arrays.copyOfRange()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,14 +5,21 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {4, 5, 6, 7, 8, 9, 10, 11, 12};
-        System.out.println(Arrays.toString(s.solution(3, arr)));
+        int[] arr = {5, 2, 1, 7, 5};
+        System.out.println(Arrays.toString(s.solution(arr, 2)));
         // 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열
     }
 }
 
 class Solution {
-    public int[] solution(int n, int[] numlist) {
-        return Arrays.stream(numlist).filter(num -> num % n == 0).toArray();
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length-(n-1)];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[n-1];
+            n++;
+        }
+
+        return answer;
     }
 }
