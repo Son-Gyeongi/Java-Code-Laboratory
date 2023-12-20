e6b41618f9e0262efd47d554e6c60f19a9aebece
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-20 22:46:28 +0900

    부분 문자열 이어 붙여 문자열 만들기 - 배열 잘 보기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,28 +1,23 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(0, "wsdawsdassw"));
+        String[] strArr = {"progressive", "hamburger", "hammer", "ahocorasick"};
+        int[][] intArr = {{0, 4}, {1, 2}, {3, 5}, {7, 7}};
+        System.out.println(s.solution(strArr, intArr));
         // 문자열들이 담긴 리스트가 주어졌을 때, 모든 문자열들을 순서대로 합친 문자열을 꼬리 문자열
     }
 }
 
 class Solution {
-    public int solution(int n, String control) {
-        int answer = n;
+    public String solution(String[] my_strings, int[][] parts) {
+        String answer = "";
 
-        /*
-        "w" : n이 1 커집니다.
-        "s" : n이 1 작아집니다.
-        "d" : n이 10 커집니다.
-        "a" : n이 10 작아집니다.
-         */
-        for (int i = 0; i < control.length(); i++) {
-            if (control.charAt(i) == 'w') answer += 1;
-            else if (control.charAt(i) == 's') answer -= 1;
-            else if (control.charAt(i) == 'd') answer += 10;
-            else answer -= 10;
+        for (int i = 0; i < my_strings.length; i++) {
+            answer += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
         }
 
         return answer;
