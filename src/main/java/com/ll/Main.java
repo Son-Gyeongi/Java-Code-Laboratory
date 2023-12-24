513f25356120486e8825a13d9b3b54a23a274c60
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-25 01:03:59 +0900

    글자 이어 붙여 문자열 만들기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,21 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {1, 2, 3, 100, 99, 98};
-        System.out.println(Arrays.toString(s.solution(arr, 3)));
+        int[] index_list = {16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7};
+        System.out.println(s.solution("cvsgiorszzzmrpaqpe", index_list));
     }
 }
 
 class Solution {
-    public int[] solution(int[] arr, int k) {
-        return Arrays.stream(arr).map(i -> k % 2 == 1 ? i * k : i + k).toArray();
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+        for (int index : index_list) {
+            answer += my_string.charAt(index);
+        }
+
+        return answer;
     }
 }
