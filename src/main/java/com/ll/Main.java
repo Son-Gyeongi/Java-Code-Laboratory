39c1feeec8ed15477ed21d37cec7add2b7f8c569
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-27 07:45:24 +0900

    원하는 문자열 찾기 - toUpperCase(), contains()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,12 +3,15 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("banana", "ban"));
+        System.out.println(s.solution("AbCdEfG", "aBc"));
     }
 }
 
 class Solution {
-    public int solution(String my_string, String is_prefix) {
-        return my_string.startsWith(is_prefix) ? 1 : 0;
+    public int solution(String myString, String pat) {
+        String myString_upperCase = myString.toUpperCase();
+        String pat_upperCase = pat.toUpperCase();
+
+        return myString_upperCase.contains(pat_upperCase) ? 1 : 0;
     }
 }
