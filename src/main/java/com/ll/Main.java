7b0117337a5dc141ee1192eb6ad9aa1c72845dc7
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-27 07:22:31 +0900

    접두사인지 확인하기 - startsWith()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,13 +3,12 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("banana", "nan"));
+        System.out.println(s.solution("banana", "ban"));
     }
 }
 
 class Solution {
-    public int solution(String my_string, String is_suffix) {
-        return !my_string.contains(is_suffix) ? 0 :
-                my_string.charAt(my_string.length() - 1) != is_suffix.charAt(is_suffix.length() - 1) ? 0 : 1;
+    public int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
