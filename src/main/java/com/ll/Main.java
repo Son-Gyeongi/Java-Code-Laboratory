fe736b61d60673c47b962723aadc745c4c033b41
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-28 06:50:19 +0900

    특정한 문자를 대문자로 바꾸기 - replace(), toUpperCase()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,17 +3,12 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(2, 91));
+        System.out.println(s.solution("programmers", "p"));
     }
 }
 
 class Solution {
-    public int solution(int a, int b) {
-
-        String ab = "" + a + b;
-        int intab = Integer.parseInt(ab);
-        int multiab = 2 * a * b;
-
-        return Math.max(intab, multiab);
+    public String solution(String my_string, String alp) {
+        return my_string.replace(alp, alp.toUpperCase());
     }
 }
