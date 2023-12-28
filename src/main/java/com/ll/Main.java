d9e92b7b917dcb89f5270586323550fc4bc12988
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-29 06:16:29 +0900

    뒤에서 5등까지 - Arrays.stream().sorted().limit().toArray();

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,21 +1,18 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] num_list = {5, 2, 1, 7, 5};
-        System.out.println(Arrays.toString(s.solution(num_list, 3)));
+        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
+        System.out.println(Arrays.toString(s.solution(num_list)));
     }
 }
 
 class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] ints = Arrays.copyOfRange(num_list, n, num_list.length);
-        int[] ints1 = Arrays.copyOfRange(num_list, 0, n);
-
-        return IntStream.concat(Arrays.stream(ints), Arrays.stream(ints1)).toArray();
+    public int[] solution(int[] num_list) {
+        int[] array = Arrays.stream(num_list).sorted().toArray();
+        return Arrays.copyOfRange(array, 0, 5);
     }
 }
