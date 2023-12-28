189355867f533762bd492731d0d90ca0e049669d
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-28 21:19:48 +0900

    순서 바꾸기 - Arrays.copyOfRange(), IntStream.concat()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,21 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("programmers", "p"));
+        int[] num_list = {5, 2, 1, 7, 5};
+        System.out.println(Arrays.toString(s.solution(num_list, 3)));
     }
 }
 
 class Solution {
-    public String solution(String my_string, String alp) {
-        return my_string.replace(alp, alp.toUpperCase());
+    public int[] solution(int[] num_list, int n) {
+        int[] ints = Arrays.copyOfRange(num_list, n, num_list.length);
+        int[] ints1 = Arrays.copyOfRange(num_list, 0, n);
+
+        return IntStream.concat(Arrays.stream(ints), Arrays.stream(ints1)).toArray();
     }
 }
