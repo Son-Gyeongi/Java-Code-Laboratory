ed29b7c1ff08f25506732e95a0580f38538dac4f
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-30 06:41:00 +0900

    홀수 vs 짝수 - Math.max()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,18 +1,24 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] num_list = {12, 4, 15, 46, 38, 1, 14};
-        System.out.println(Arrays.toString(s.solution(num_list)));
+        int[] num_list = {4, 2, 6, 1, 7, 6};
+        System.out.println(s.solution(num_list));
     }
 }
 
 class Solution {
-    public int[] solution(int[] num_list) {
-        int[] array = Arrays.stream(num_list).sorted().toArray();
-        return Arrays.copyOfRange(array, 0, 5);
+    public int solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            // 배열 인덱스가 0,2,4.. -> 홀수 번째로 나오기
+            if (i % 2 == 0) odd += num_list[i];
+            else even += num_list[i];
+        }
+
+        return Math.max(odd, even);
     }
 }
