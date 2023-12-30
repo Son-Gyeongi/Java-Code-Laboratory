654d0ecc57e61420250b28056cf5ae029b237d38
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-31 06:49:30 +0900

    암호 해독

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,25 +3,21 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(999));
+        System.out.println(s.solution("dfjardstddetckdaccccdegk", 4));
     }
 }
 
 class Solution {
-    public int solution(int hp) {
-        int answer = 0;
+    public String solution(String cipher, int code) {
+        StringBuilder answer = new StringBuilder();
+        int index = code -1;
+        int strLength = cipher.length() / code;
 
-        // 장군개미는 5의 공격력을, 병정개미는 3의 공격력을 일개미는 1의 공격력
-        if (hp / 5 > 0) {
-            answer = hp / 5;
-            hp %= 5;
+        for (int i = 0; i < strLength; i++) {
+            answer.append(cipher.charAt(index));
+            index += code;
         }
-        if (hp / 3 > 0) {
-            answer += (hp / 3);
-            hp %= 3;
-        }
-        answer += hp;
 
-        return answer;
+        return answer.toString();
     }
 }
