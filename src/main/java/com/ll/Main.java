32e77fb4d4d6f0df3984d0dd787c55e89862521b
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-31 06:07:23 +0900

    개미 군단

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,29 +3,25 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("205"));
+        System.out.println(s.solution(999));
     }
 }
 
 class Solution {
-    public String solution(String rsp) {
-        StringBuilder answer = new StringBuilder();
+    public int solution(int hp) {
+        int answer = 0;
 
-        // 가위는 2 바위는 0 보는 5로 표현
-        for (int i = 0; i < rsp.length(); i++) {
-            switch (String.valueOf(rsp.charAt(i))) {
-                case "2" :
-                    answer.append("0");
-                    break;
-                case "0" :
-                    answer.append("5");
-                    break;
-                case "5" :
-                    answer.append("2");
-                    break;
-            }
+        // 장군개미는 5의 공격력을, 병정개미는 3의 공격력을 일개미는 1의 공격력
+        if (hp / 5 > 0) {
+            answer = hp / 5;
+            hp %= 5;
         }
+        if (hp / 3 > 0) {
+            answer += (hp / 3);
+            hp %= 3;
+        }
+        answer += hp;
 
-        return answer.toString();
+        return answer;
     }
 }
