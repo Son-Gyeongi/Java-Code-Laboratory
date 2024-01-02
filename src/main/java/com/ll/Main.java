300ed600f4284a191e41ee07fae5c23e0dc8d496
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-03 06:02:27 +0900

    가장 큰 수 찾기 - findFirst().getAsInt() 배열의 인덱스값을 찾는다.

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,26 +1,27 @@
 package com.ll;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(10)));
+        int[] array = {1, 8, 3};
+        System.out.println(Arrays.toString(s.solution(array)));
     }
 }
 
 class Solution {
-    public int[] solution(int n) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
 
-        while (n != 1) {
-            answer.add(n);
-            n = n % 2 == 0 ? n / 2 : 3 * n + 1;
-        }
-        answer.add(n);
+        int[] sortedArr = Arrays.stream(array).sorted().toArray();
 
-        return answer.stream().mapToInt(i -> i).toArray();
+        answer[0] = sortedArr[sortedArr.length - 1];
+        answer[1] = IntStream.rangeClosed(0, array.length)
+                .filter(i -> answer[0] == array[i])
+                .findFirst().getAsInt();
+
+        return answer;
     }
 }
