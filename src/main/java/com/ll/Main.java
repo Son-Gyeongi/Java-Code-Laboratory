f85a95c342cfa408e31fe69cf756bbc69f8b4d27
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-02 10:10:34 +0900

    최댓값 만들기 (2) - Arrays.sort(), Math.max()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,26 +1,26 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] box = {10, 8, 6}; // 가로, 세로, 높이
-        System.out.println(s.solution(box, 3));
+        int[] numbers = {1, 2, -3, 4, -5};
+        System.out.println(s.solution(numbers));
     }
 }
 
 class Solution {
-    public int solution(int[] box, int n) {
+    public int solution(int[] numbers) {
+        int answer;
+        int max = numbers[0] * numbers[1];
 
-        // 직육면체의 부피를 구하는 방법은 가로 X 세로 X 높이로 밑면의 넓이 X 높이
-        return Arrays.stream(box).map(i -> i / n)
-                .reduce(1, (i, acc) -> i * acc);
-        /*
-        가로가 10 이면 길이가 3인 주사위가 3개 들어간다.
-        세로로 8이면 길이가 3인 주사위가 2개 들어간다.
-        높이가 6이면 길이가 3인 주사위가 2개 들어간다.
-        3 X 2 X 2 = 12 이다.
-         */
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i == j) break; // 같은 인덱스는 거른다.
+                answer = numbers[i] * numbers[j];
+                max = Math.max(max, answer);  // 최대값
+            }
+        }
+
+        return max;
     }
 }
