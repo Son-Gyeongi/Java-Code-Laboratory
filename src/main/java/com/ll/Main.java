f3f39fb4179811ce46d0d171de46a9e214578f04
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-05 17:25:33 +0900

    가까운 1 찾기 - IntStream.range().filter.findFirst().orElse()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,16 +1,21 @@
 package com.ll;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("0010"));
+        int[] arr = {1, 1, 1, 1, 0};
+        System.out.println(s.solution(arr, 3));
     }
 }
 
 class Solution {
-    public String solution(String n_str) {
-        int numStr = Integer.parseInt(n_str);
-
-        return String.valueOf(numStr);
+    public int solution(int[] arr, int idx) {
+        return IntStream.range(0, arr.length)
+                .filter(i -> idx <= i)
+                .filter(i -> arr[i] == 1)
+                .findFirst()
+                .orElse(-1);
     }
 }
