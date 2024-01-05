eb7c799bd75a1031be7cd3c6f9e00b3bf7067eae
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-05 17:15:25 +0900

    0 떼기 - Integer.parseInt(), String.valueOf() / 생각의 전환이 필요한(간단하게 생각하자)

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,27 +1,16 @@
 package com.ll;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] array = {1, 8, 3};
-        System.out.println(Arrays.toString(s.solution(array)));
+        System.out.println(s.solution("0010"));
     }
 }
 
 class Solution {
-    public int[] solution(int[] array) {
-        int[] answer = new int[2];
+    public String solution(String n_str) {
+        int numStr = Integer.parseInt(n_str);
 
-        int[] sortedArr = Arrays.stream(array).sorted().toArray();
-
-        answer[0] = sortedArr[sortedArr.length - 1];
-        answer[1] = IntStream.rangeClosed(0, array.length)
-                .filter(i -> answer[0] == array[i])
-                .findFirst().getAsInt();
-
-        return answer;
+        return String.valueOf(numStr);
     }
 }
