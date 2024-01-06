5dc8b9b60c73d8fe8a1c4803c9241142987de686
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-07 08:44:32 +0900

    x 사이의 개수

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,22 +1,32 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution("hi12392")));
+        System.out.println(Arrays.toString(s.solution("xabcxdefxghi")));
     }
 }
 
 class Solution {
-    public int[] solution(String my_string) {
-        String str = my_string.replaceAll("[a-zA-Z]", "");
-        String[] strArr = str.split("");
+    public int[] solution(String myString) {
+        List<Integer> answer = new ArrayList<>();
+//        char[] charArray = myString.toCharArray(); // [o, x, o, o, x, o, x, x, o, x]
+//        String[] split = myString.split("x"); // "oxooxoxxox" [o, oo, o, , o]
+//        System.out.println(Arrays.toString(split)); // "xabcxdefxghi" [, abc, def, ghi]
 
-        return Arrays.stream(strArr).mapToInt(Integer::parseInt)
-                .filter(i -> 0 <= i && i <= 9)
-                .sorted()
-                .toArray();
+//        if (charArray[0] == 'x') answer.add(0);
+//        if (myString.charAt(0) == 'x') answer.add(0);
+
+        Arrays.stream(myString.split("x"))
+                .map(i -> answer.add(i.length())).toArray();
+
+//        if (charArray[charArray.length -1] == 'x') answer.add(0);
+        if (myString.charAt(myString.length() -1) == 'x') answer.add(0);
+
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
