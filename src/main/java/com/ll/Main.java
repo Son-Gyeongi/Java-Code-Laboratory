278b015846f1783eeca2bb4a15eadb0049aa32d7
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-08 05:43:05 +0900

    특별한 이차원 배열 2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,22 +1,29 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution("banana")));
+//        int[][] arr = {{5, 192, 33},
+//                {192, 72, 95},
+//                {33, 95, 999}};
+        int[][] arr = {{19, 498, 258, 587},
+                {63, 93, 7, 754},
+                {258, 7, 1000, 723},
+                {587, 754, 723, 81}};
+        System.out.println(s.solution(arr));
     }
 }
 
 class Solution {
-    public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
+    public int solution(int[][] arr) {
+        int answer = 1;
 
-        for (int i = 0; i < my_string.length(); i++) {
-            answer[i] = my_string.substring(i);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) return 0;
+            }
         }
 
-        return Arrays.stream(answer).sorted().toArray(String[]::new);
+        return answer;
     }
 }
