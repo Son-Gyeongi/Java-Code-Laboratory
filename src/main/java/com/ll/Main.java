f23c33c3ff1e389de4b72af40da6a61b78abdc2a
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-08 05:25:43 +0900

    접미사 배열 - Arrays.sort()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,20 +1,22 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] names = {"nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"};
-        System.out.println(Arrays.toString(s.solution(names)));
+        System.out.println(Arrays.toString(s.solution("banana")));
     }
 }
 
 class Solution {
-    public String[] solution(String[] names) {
-        return IntStream.range(0, names.length).filter(i -> i % 5 == 0)
-                .mapToObj(i -> names[i])
-                .toArray(String[]::new); // IntStream요소가 문자열 배열로 변경됨
+    public String[] solution(String my_string) {
+        String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            answer[i] = my_string.substring(i);
+        }
+
+        return Arrays.stream(answer).sorted().toArray(String[]::new);
     }
 }
