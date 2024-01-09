507312564e31407e949333c58843959838b01fd6
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-09 17:38:16 +0900

    수 조작하기 2

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,19 +1,36 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] strArr = {"and", "notad", "abcd"};
-        System.out.println(Arrays.toString(s.solution(strArr)));
+        int[] numLog = {0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1}; // 각각 연산한 결과
+        System.out.println(s.solution(numLog));
     }
 }
 
 class Solution {
-    public String[] solution(String[] strArr) {
-        return Arrays.stream(strArr)
-                .filter(str -> !str.contains("ad"))
-                .toArray(String[]::new);
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < numLog.length - 1; i++) {
+            int num = numLog[i + 1] - numLog[i];
+
+            switch (num) {
+                case 1:
+                    answer.append("w");
+                    break;
+                case -1:
+                    answer.append("s");
+                    break;
+                case 10:
+                    answer.append("d");
+                    break;
+                case -10:
+                    answer.append("a");
+                    break;
+            }
+        }
+
+        return answer.toString();
     }
 }
