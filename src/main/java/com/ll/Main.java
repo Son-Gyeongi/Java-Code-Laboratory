3d73f0d7bdb891f23924ef570a4d14d0ab34396b
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-09 13:07:28 +0900

    ad 제거하기 - str.contains()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,22 +5,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        // 다차원 배열 출력
-        System.out.println(Arrays.deepToString(s.solution(3)));
+        String[] strArr = {"and", "notad", "abcd"};
+        System.out.println(Arrays.toString(s.solution(strArr)));
     }
 }
 
 class Solution {
-    public int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) answer[i][j] = 1;
-                else answer[i][j] = 0;
-            }
-        }
-
-        return answer;
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+                .filter(str -> !str.contains("ad"))
+                .toArray(String[]::new);
     }
 }
