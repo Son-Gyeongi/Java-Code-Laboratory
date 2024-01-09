265fc3f0b941239a8c1f40f801ce09d866aab523
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-09 13:13:06 +0900

    덧셈식 출력하기 - printf(%d, a)

--- src/main/java/com/ll/Solution1.java
+++ src/main/java/com/ll/Solution1.java
@@ -5,13 +5,9 @@ import java.util.Scanner;
 public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        System.out.printf("%d + %d = %d", a, b, a + b);
     }
 }
