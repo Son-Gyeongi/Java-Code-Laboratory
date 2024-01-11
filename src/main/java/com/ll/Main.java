a9424e7da91d9f48d987a9010a860f443241ad52
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-11 21:34:06 +0900

    배열 만들기 3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,31 +1,30 @@
 package com.ll;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(24)));
+        int[] arr = {1, 2, 3, 4, 5};
+        int[][] intervals = {{1, 3}, {0, 4}};
+        System.out.println(Arrays.toString(s.solution(arr, intervals)));
     }
 }
 
 class Solution {
-    public int[] solution(int n) {
-        List<Integer> answerList = new ArrayList<>();
+    public int[] solution(int[] arr, int[][] intervals) {
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = n; j > 0; j--) {
-                if (i * j == n) answerList.add(i);
-            }
-        }
+        int[] ints = Arrays.copyOfRange(arr, intervals[0][0], intervals[0][1] + 1);
+        int[] intArr = Arrays.copyOfRange(arr, intervals[1][0], intervals[1][1] + 1);
 
-        // List를 int 배열로 변환
-        int[] answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
-        }
+        // 두 배열의 길이 합치기
+        int[] answer = new int[ints.length + intArr.length];
+
+        // 첫 번째 배열 복사
+        System.arraycopy(ints, 0, answer, 0, ints.length);
+
+        // 두 번째 배열 복사
+        System.arraycopy(intArr, 0, answer, ints.length, intArr.length);
 
         return answer;
     }
