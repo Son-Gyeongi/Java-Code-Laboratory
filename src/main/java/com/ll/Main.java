2c62aae065cde159234649b9ff50c89389416118
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-11 20:41:37 +0900

    약수 구하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,19 +1,32 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] arr = {"a", "b", "c"};
-        System.out.println(s.solution(arr));
+        System.out.println(Arrays.toString(s.solution(24)));
     }
 }
 
 class Solution {
-    public String solution(String[] arr) {
-//        return Arrays.stream(arr).collect(Collectors.joining());
-        return String.join("", arr);
+    public int[] solution(int n) {
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > 0; j--) {
+                if (i * j == n) answerList.add(i);
+            }
+        }
+
+        // List를 int 배열로 변환
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
     }
 }
