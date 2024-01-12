b5e08a4684f9414004d4819d2c3a9f8eddc4bfe5
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-13 07:21:22 +0900

    외계행성의 나이 - 깔끔하게 a부터j까지 배열에 넣으면 되네!

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,27 +1,34 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] numbers = {4, 455, 6, 4, -1, 45, 6};
-        System.out.println(Arrays.toString(s.solution(numbers, "left")));
+        System.out.println(s.solution(23)); // cd
     }
 }
 
 class Solution {
-    public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
+    public String solution(int age) {
+        StringBuilder answer = new StringBuilder();
 
-        if (direction.equals("right")) {
-            if (numbers.length - 1 >= 0) System.arraycopy(numbers, 0, answer, 1, numbers.length - 1);
-            answer[0] = numbers[numbers.length - 1];
-        } else {
-            if (numbers.length - 1 >= 0) System.arraycopy(numbers, 1, answer, 0, numbers.length - 1);
-            answer[answer.length - 1] = numbers[0];
+        // a는 0, b는 1, c는 2, ..., j는 9
+        String str = age + "";
+
+        for (int i = 0; i < str.length(); i++) {
+            switch (str.charAt(i)) {
+                case '0' : answer.append("a"); break;
+                case '1' : answer.append("b"); break;
+                case '2' : answer.append("c"); break;
+                case '3' : answer.append("d"); break;
+                case '4' : answer.append("e"); break;
+                case '5' : answer.append("f"); break;
+                case '6' : answer.append("g"); break;
+                case '7' : answer.append("h"); break;
+                case '8' : answer.append("i"); break;
+                case '9' : answer.append("j"); break;
+            }
         }
 
-        return answer;
+        return answer.toString();
     }
 }
