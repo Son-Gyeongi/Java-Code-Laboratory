fc009a3fa0059676cc10e1e06c4162193f64664c
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-15 06:46:15 +0900

    9로 나눈 나머지 - Arrays.stream(배열).mapToInt(Integer::parseInt).sum();

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,22 +1,19 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(2, 6, 1));
+        System.out.println(s.solution("78720646226947352489"));
     }
 }
 
 class Solution {
-    public int solution(int a, int b, int c) {
-        boolean option1 = a == b;
-        boolean option2 = b == c;
-        boolean option3 = c == a;
+    public int solution(String number) {
+        String[] split = number.split("");
+        int sum = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
 
-        if (option1 && option2) { // option3은 항상 true
-            return (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
-        } else if (option1 || option2 || option3) {
-            return (a + b + c) * ((a * a) + (b * b) + (c * c));
-        } else return a + b + c;
+        return sum % 9;
     }
 }
