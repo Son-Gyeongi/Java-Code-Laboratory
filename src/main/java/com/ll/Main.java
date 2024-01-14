e8711ff8e4bf5fd68c16892c6fcb1c55cd5fa03a
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-14 10:39:39 +0900

    comment : 주석 변경

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -13,7 +13,7 @@ class Solution {
         boolean option2 = b == c;
         boolean option3 = c == a;
 
-        if (option1 && option2) { // opiton3은 항상 true
+        if (option1 && option2) { // option3은 항상 true
             return (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
         } else if (option1 || option2 || option3) {
             return (a + b + c) * ((a * a) + (b * b) + (c * c));
