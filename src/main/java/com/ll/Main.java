dacdfd7bdd1f190770a95c146308a69e7968efb0
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-15 07:15:19 +0900

    369게임

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("78720646226947352489"));
+        System.out.println(s.solution(29403));
     }
 }
 
 class Solution {
-    public int solution(String number) {
-        String[] split = number.split("");
-        int sum = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
+    public int solution(int order) {
+        String[] split = String.valueOf(order).split("");
 
-        return sum % 9;
+        return (int) Arrays.stream(split).mapToInt(Integer::parseInt)
+                .filter(i -> i % 3 == 0 && i != 0)
+                .count();
     }
 }
