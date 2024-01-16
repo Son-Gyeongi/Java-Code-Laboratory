1cb3d3a86596ad55fba304ad3e3bc6da63f3156a
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-17 06:23:20 +0900

    문자열 정렬하기 (2) - Arrays.stream(배열).map().sorted().collect(Collectors.joining());

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,20 +1,21 @@
 package com.ll;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(29403));
+        System.out.println(s.solution("Bcad"));
     }
 }
 
 class Solution {
-    public int solution(int order) {
-        String[] split = String.valueOf(order).split("");
-
-        return (int) Arrays.stream(split).mapToInt(Integer::parseInt)
-                .filter(i -> i % 3 == 0 && i != 0)
-                .count();
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split("")) // 문자열을 한 글자씩 분리하여 Stream으로 변환
+                .map(str -> str.toLowerCase()) // 각 문자를 소문자로 변환
+                .sorted().collect(Collectors.joining());
+        // 문자를 정렬
+        // 정렬된 문자를 하나의 문자열로 결합
     }
 }
