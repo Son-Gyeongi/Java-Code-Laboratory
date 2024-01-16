8775c5c63ed68891b2e03d91b0b4cb4ded475448
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-17 07:22:20 +0900

    세로 읽기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,21 +1,20 @@
 package com.ll;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("Bcad"));
+        System.out.println(s.solution("ihrhbakrfpndopljhygc", 4, 2));
     }
 }
 
 class Solution {
-    public String solution(String my_string) {
-        return Arrays.stream(my_string.split("")) // 문자열을 한 글자씩 분리하여 Stream으로 변환
-                .map(str -> str.toLowerCase()) // 각 문자를 소문자로 변환
-                .sorted().collect(Collectors.joining());
-        // 문자를 정렬
-        // 정렬된 문자를 하나의 문자열로 결합
+    public String solution(String my_string, int m, int c) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer.append(my_string.charAt(i));
+        }
+
+        return answer.toString();
     }
 }
