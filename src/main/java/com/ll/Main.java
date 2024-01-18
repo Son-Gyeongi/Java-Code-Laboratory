fa2942073f53b6dd2ddd9dc5ddfb9a9c8596bbc9
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-18 20:31:12 +0900

    등차수열의 특정한 항만 더하기 - answer +=  a + (d*i);

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,18 +3,21 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("ihrhbakrfpndopljhygc", 4, 2));
+        boolean[] included = {true, false, false, true, true};
+        System.out.println(s.solution(3,4, included));
     }
 }
 
 class Solution {
-    public String solution(String my_string, int m, int c) {
-        StringBuilder answer = new StringBuilder();
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        int cnt = 0;
 
-        for (int i = c - 1; i < my_string.length(); i += m) {
-            answer.append(my_string.charAt(i));
+        for (int i = a; cnt < included.length; i += d) {
+            if (included[cnt]) answer += i;
+            ++cnt;
         }
 
-        return answer.toString();
+        return answer;
     }
 }
