ee7ae0d910bf3d02f05979d4e9ff8b1b67f30087
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-20 07:27:50 +0900

    수열과 구간 쿼리 1 - queries[i][0] ≤ arr인덱스 ≤ queries[i][1]

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,21 +1,27 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("people"));
+        int[] arr = {0, 1, 2, 3, 4};
+        int[][] queries = {{0, 1},
+                            {1, 2},
+                            {2, 3}};
+        System.out.println(Arrays.toString(s.solution(arr, queries)));
     }
 }
 
 class Solution {
-    public String solution(String my_string) {
-        String answer = "";
-
-        for (int i = 0; i < my_string.length(); i++) {
-            String str = String.valueOf(my_string.charAt(i));
-            if (!answer.contains(str)) answer += str;
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (queries[i][0] <= j && j <= queries[i][1])
+                    arr[j] += 1;
+            }
         }
 
-        return answer;
+        return arr;
     }
 }
