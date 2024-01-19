8ef062d7bbdd400bc82d9a106e389aee95b1df12
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-20 06:18:44 +0900

    중복된 문자 제거

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,16 +3,17 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("aaaaa", "bbbbb"));
+        System.out.println(s.solution("people"));
     }
 }
 
 class Solution {
-    public String solution(String str1, String str2) {
+    public String solution(String my_string) {
         String answer = "";
 
-        for (int i = 0; i < str1.length(); i++) {
-            answer += ("" + str1.charAt(i) + str2.charAt(i));
+        for (int i = 0; i < my_string.length(); i++) {
+            String str = String.valueOf(my_string.charAt(i));
+            if (!answer.contains(str)) answer += str;
         }
 
         return answer;
