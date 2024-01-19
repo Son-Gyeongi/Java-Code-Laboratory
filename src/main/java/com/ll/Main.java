8345d9caa4d1044525eb6321689cc02adf785800
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-19 09:43:56 +0900

    이차원 배열 대각선 순회하기 - 문제에 답이 있다.

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,19 +3,22 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        boolean[] included = {true, false, false, true, true};
-        System.out.println(s.solution(3,4, included));
+        int[][] board = {{0, 1, 2},
+                        {1, 2, 3},
+                        {2, 3, 4},
+                        {3, 4, 5}};
+        System.out.println(s.solution(board, 2));
     }
 }
 
 class Solution {
-    public int solution(int a, int d, boolean[] included) {
+    public int solution(int[][] board, int k) {
         int answer = 0;
-        int cnt = 0;
 
-        for (int i = a; cnt < included.length; i += d) {
-            if (included[cnt]) answer += i;
-            ++cnt;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (i + j <= k) answer += board[i][j];
+            }
         }
 
         return answer;
