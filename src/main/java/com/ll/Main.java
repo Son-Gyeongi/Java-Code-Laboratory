4e02766389505fda45b3e8ddb884d908a6904a19
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-22 08:02:36 +0900

    특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 - 문자열에서 특정 문자열을 찾을 때 가장 마지막 문자열의 인덱스를 찾는 건 lastIndexOf()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,31 +1,16 @@
 package com.ll;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {3, 2, 4, 1, 3};
-        boolean[] flag = {true, false, true, false, false};
-        System.out.println(Arrays.toString(s.solution(arr, flag)));
+        System.out.println(s.solution("AAAAaaaa", "a"));
     }
 }
 
 class Solution {
-    public int[] solution(int[] arr, boolean[] flag) {
-        List<Integer> answer = new ArrayList<>();
+    public String solution(String myString, String pat) {
+        int strLastIdx = myString.lastIndexOf(pat) + pat.length();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (flag[i]) {
-                int repeat = arr[i] * 2;
-                for (int j = 0; j < repeat; j++) answer.add(arr[i]);
-            } else {
-                for (int j = 0; j < arr[i]; j++) answer.remove(answer.size() - 1);
-            }
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return myString.substring(0, strLastIdx);
     }
 }
