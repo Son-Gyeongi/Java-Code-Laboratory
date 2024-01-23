648cf42743cfdc08b4de1f7889ce09c7c31825e7
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-24 07:50:01 +0900

    문자열이 몇 번 등장하는지 세기 - substring().startsWith()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,24 +3,18 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] num_list = {12, 4, 15, 1, 14};
-        System.out.println(s.solution(num_list));
+        System.out.println(s.solution("banana", "ana"));
     }
 }
 
 class Solution {
-    public int solution(int[] num_list) {
-        int count = 0;
+    public int solution(String myString, String pat) {
+        int answer = 0;
 
-        for (int num : num_list) {
-            int n = num;
-            while (n > 1) {
-                if (n % 2 == 0) n /= 2;
-                else n = (n - 1) / 2;
-                count++;
-            }
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.substring(i).startsWith(pat)) answer++;
         }
 
-        return count;
+        return answer;
     }
 }
