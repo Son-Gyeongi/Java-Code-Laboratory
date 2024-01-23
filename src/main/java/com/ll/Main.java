8ff5e209bdbdc82b44c48094a082682b2358d00c
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-24 07:18:22 +0900

    1로 만들기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,21 +1,26 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] intStrs = {"0123456789", "9876543210", "9999999999999"};
-        System.out.println(Arrays.toString(s.solution(intStrs, 50000, 5, 5)));
+        int[] num_list = {12, 4, 15, 1, 14};
+        System.out.println(s.solution(num_list));
     }
 }
 
 class Solution {
-    public int[] solution(String[] intStrs, int k, int s, int l) {
-        return Arrays.stream(intStrs)
-                .map(str -> str.substring(s, s + l))
-                .mapToInt(Integer::parseInt)
-                .filter(i -> k < i)
-                .toArray();
+    public int solution(int[] num_list) {
+        int count = 0;
+
+        for (int num : num_list) {
+            int n = num;
+            while (n > 1) {
+                if (n % 2 == 0) n /= 2;
+                else n = (n - 1) / 2;
+                count++;
+            }
+        }
+
+        return count;
     }
 }
