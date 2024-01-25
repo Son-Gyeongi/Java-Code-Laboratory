abe4b97eef7b036ec65ff33dc3b6072121170f2d
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-26 06:17:55 +0900

    간단한 논리 연산 - &&(AND), ||(OR)

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,23 +3,15 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(7));
+        System.out.println(s.solution(true, false, false, false));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int answer = 1;
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = false;
 
-        if (n == 3628800) return 10;
-        else if (362880 <= n && n < 3628800) return 9;
-        else if (40320 <= n && n < 362880) return 8;
-        else if (5040 <= n && n < 40320) return 7;
-        else if (720 <= n && n < 5040) return 6;
-        else if (120 <= n && n < 720) return 5;
-        else if (24 <= n && n < 120) return 4;
-        else if (6 <= n && n < 24) return 3;
-        else if (2 <= n && n < 6) return 2;
+        if ((x1 || x2) && (x3 || x4)) return true;
 
         return answer;
     }
