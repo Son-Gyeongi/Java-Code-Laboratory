0d827b54d69d2117eb8e0b2039047acc171237c3
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-26 06:56:31 +0900

    A로 B 만들기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,18 +1,20 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(true, false, false, false));
+        System.out.println(s.solution("wrong", "wrogn"));
     }
 }
 
 class Solution {
-    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = false;
+    public int solution(String before, String after) {
+        String beforeStr = Arrays.stream(before.split("")).sorted().collect(Collectors.joining());
+        String afterStr = Arrays.stream(after.split("")).sorted().collect(Collectors.joining());
 
-        if ((x1 || x2) && (x3 || x4)) return true;
-
-        return answer;
+        return beforeStr.equals(afterStr) ? 1 : 0;
     }
 }
