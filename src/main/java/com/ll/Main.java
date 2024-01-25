e706861f50b07f7186f820f2e9e6d42aeec68056
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-25 09:48:02 +0900

    모스부호 (1) - Map 사용

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,18 +1,44 @@
 package com.ll;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("banana", "ana"));
+        System.out.println(s.solution(".... . .-.. .-.. ---"));
     }
 }
 
 class Solution {
-    public int solution(String myString, String pat) {
-        int answer = 0;
+    public String solution(String letter) {
+        String answer = "";
 
-        for (int i = 0; i < myString.length(); i++) {
-            if (myString.substring(i).startsWith(pat)) answer++;
+        /*
+        morse = {
+                    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+                    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+                    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+                    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+                    '-.--':'y','--..':'z'
+                }
+         */
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.",
+                            "--.", "....", "..", ".---", "-.-", ".-..",
+                            "--", "-.", "---", ".--.", "--.-", ".-.",
+                            "...", "-", "..-", "...-", ".--", "-..-",
+                            "-.--", "--.."};
+
+        Map<String, Character> morseStr = new HashMap<>();
+
+        for (int i = 0; i < morse.length; i++) {
+            morseStr.put(morse[i], (char) (97 + i));
+        }
+
+        String[] splitLetter = letter.split(" ");
+
+        for (String s : splitLetter) {
+            answer += morseStr.get(s);
         }
 
         return answer;
