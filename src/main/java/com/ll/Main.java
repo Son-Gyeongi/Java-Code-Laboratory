b8013e95819dfcd7d3767f0592993e4a0ad83a7d
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-27 21:51:13 +0900

    숨어있는 숫자의 덧셈 (2) - my_string.replaceAll([^0-9], )

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,20 +1,21 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("wrong", "wrogn"));
+        System.out.println(s.solution("aAb1B2cC34oOp"));
     }
 }
 
 class Solution {
-    public int solution(String before, String after) {
-        String beforeStr = Arrays.stream(before.split("")).sorted().collect(Collectors.joining());
-        String afterStr = Arrays.stream(after.split("")).sorted().collect(Collectors.joining());
+    public int solution(String my_string) {
+        String s = my_string.replaceAll("[a-zA-Z]", " ");
 
-        return beforeStr.equals(afterStr) ? 1 : 0;
+        return Arrays.stream(s.split(" "))
+                .filter(i -> !i.isEmpty())
+                .mapToInt(Integer::valueOf)
+                .sum();
     }
 }
