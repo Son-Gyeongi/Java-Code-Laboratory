5524363613d00fc006716b6e489d62cca5c1edc3
Son-Gyeongi <thsruddl77@gmail.com>
2024-01-31 10:09:56 +0900

    배열의 원소 삭제하기 - list.remove()

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,18 +7,26 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {5, 1, 4};
-        System.out.println(Arrays.toString(s.solution(arr)));
+        int[] arr = {293, 1000, 395, 678, 94};
+        int[] delete_list = {94, 777, 104, 1000, 1, 12};
+        System.out.println(Arrays.toString(s.solution(arr, delete_list)));
     }
 }
 
 class Solution {
-    public int[] solution(int[] arr) {
-        List<Integer> list = new ArrayList<>();
+    public int[] solution(int[] arr, int[] delete_list) {
+        List<Integer> list = new ArrayList<>(arr.length);
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i]; j++) {
-                list.add(arr[i]);
+        // int 배열을 Integer 리스트로 변환
+        for (int value : arr) {
+            list.add(value);
+        }
+
+        for (int delete : delete_list) {
+            for (int j = 0; j < list.size(); j++) {
+                if (delete == list.get(j)) {
+                    list.remove(j);
+                }
             }
         }
 
