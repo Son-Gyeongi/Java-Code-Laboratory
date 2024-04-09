1c224a96c130665dace3bd96e19e3f13bc6b5d12
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-10 00:35:57 +0900

    문자열 반복해서 출력하기

--- src/main/java/com/ll/Solution1.java
+++ src/main/java/com/ll/Solution1.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Solution1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        String str = sc.next();
+        int n = sc.nextInt();
 
-        System.out.printf("%d + %d = %d", a, b, a + b);
+        System.out.println(str.repeat(n));
     }
 }
