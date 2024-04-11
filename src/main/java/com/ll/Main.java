35cf509242b499ecd43aed3a6d8d38a69d9231f6
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-11 13:45:57 +0900

    나머지가 1이 되는 수 찾기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,24 +1,21 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] emergency = {30, 10, 23, 6, 100};
-        System.out.println(Arrays.toString(s.solution(emergency)));
+        System.out.println(s.solution(10));
     }
 }
 
 class Solution {
-    public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
+    public int solution(int n) {
+        int answer = 0;
+        int unknown = n - 1;
 
-        for (int i = 0; i < emergency.length; i++) {
-            for (int j = 0; j < emergency.length; j++) {
-                if (emergency[i] >= emergency[j]) {
-                    answer[j] += 1;
-                }
+        for (int i = 2; i <= n; i++) {
+            if (unknown % i == 0) {
+                answer = i;
+                break;
             }
         }
 
