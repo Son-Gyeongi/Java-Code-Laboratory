b0d14dacf5c652f9428dbe18c31f23ba7236bbd1
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-12 19:44:05 +0900

    약수의 합

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,23 +5,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] emergency = {30, 10, 23, 6, 100};
-        System.out.println(Arrays.toString(s.solution(emergency)));
+        System.out.println(s.solution(12));
     }
 }
 
 class Solution {
-    public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
+    public int solution(int n) {
+        int answer = 0;
 
-        for (int i = 0; i < emergency.length; i++) {
-            for (int j = 0; j < emergency.length; j++) {
-                if (emergency[i] >= emergency[j]) {
-                    answer[j] += 1;
-                }
-            }
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0) answer += i;
         }
 
-        return answer;
+        return answer+n;
     }
 }
