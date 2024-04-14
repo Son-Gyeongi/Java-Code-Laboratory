c119b19dd6e2111cec59dd713468557fe50ef01a
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-14 23:27:38 +0900

    정수 내림차순으로 배치하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,23 +1,34 @@
 package com.ll;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(2, 5)));
+        System.out.println(s.solution(118372));
     }
 }
 
 class Solution {
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        answer[0] = x;
+    public long solution(long n) {
+        long answer = 0;
+        String[] splitStr = String.valueOf(n).split("");
 
-        for (int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] + x;
+        for (int i = 0; i < splitStr.length; i++) {
+            for (int j = i + 1; j < splitStr.length; j++) {
+                if (Long.parseLong(splitStr[i]) < Long.parseLong(splitStr[j])) {
+                    String temp = splitStr[i];
+                    splitStr[i] = splitStr[j];
+                    splitStr[j] = temp;
+                }
+            }
         }
 
-        return answer;
+        String collectStr = String.join("", splitStr);
+
+        return Long.parseLong(collectStr);
     }
 }
