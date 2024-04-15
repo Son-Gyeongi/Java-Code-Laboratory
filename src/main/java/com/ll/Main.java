0e401416ebde520a93a48782aca804924c936206
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-15 11:20:13 +0900

    정수 제곱근 판별

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,34 +1,18 @@
 package com.ll;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(118372));
+        System.out.println(s.solution(121));
     }
 }
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        String[] splitStr = String.valueOf(n).split("");
+        long sqrt = (long) Math.sqrt(n);
 
-        for (int i = 0; i < splitStr.length; i++) {
-            for (int j = i + 1; j < splitStr.length; j++) {
-                if (Long.parseLong(splitStr[i]) < Long.parseLong(splitStr[j])) {
-                    String temp = splitStr[i];
-                    splitStr[i] = splitStr[j];
-                    splitStr[j] = temp;
-                }
-            }
-        }
+        if (n != Math.pow(sqrt, 2)) return -1;
 
-        String collectStr = String.join("", splitStr);
-
-        return Long.parseLong(collectStr);
+        return (long) Math.pow(sqrt + 1, 2);
     }
 }
