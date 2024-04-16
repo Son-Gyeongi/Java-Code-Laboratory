f7d5c265c7e4dda593ec694337e269442aa3613b
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-16 13:47:02 +0900

    짝수와 홀수

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,16 +3,12 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(121));
+        System.out.println(s.solution(4));
     }
 }
 
 class Solution {
-    public long solution(long n) {
-        long sqrt = (long) Math.sqrt(n);
-
-        if (n != Math.pow(sqrt, 2)) return -1;
-
-        return (long) Math.pow(sqrt + 1, 2);
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
     }
 }
