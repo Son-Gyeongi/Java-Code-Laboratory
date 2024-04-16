b7d9da9bc457721da732f11df8138aa7795a7f56
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-16 14:09:31 +0900

    lv0. 수열과 구간 쿼리 4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,14 +1,24 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(4));
+        int[] arr = {0, 1, 2, 4, 3};
+        int[][] queries = {{0, 4, 1}, {0, 3, 2}, {0, 3, 3}};
+        System.out.println(Arrays.toString(s.solution(arr, queries)));
     }
 }
 
 class Solution {
-    public String solution(int num) {
-        return num % 2 == 0 ? "Even" : "Odd";
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            for (int j = query[0]; j <= query[1]; j++) {
+                if (j % query[2] == 0) arr[j] += 1;
+            }
+        }
+
+        return arr;
     }
 }
