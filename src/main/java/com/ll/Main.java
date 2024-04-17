8d997ec7b49d7d07ece1ad42a1cda2d681990af2
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-17 20:42:17 +0900

    lv1. 두 정수 사이의 합

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,24 +1,26 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {0, 1, 2, 4, 3};
-        int[][] queries = {{0, 4, 1}, {0, 3, 2}, {0, 3, 3}};
-        System.out.println(Arrays.toString(s.solution(arr, queries)));
+        System.out.println(s.solution(5, 3));
     }
 }
 
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        for (int[] query : queries) {
-            for (int j = query[0]; j <= query[1]; j++) {
-                if (j % query[2] == 0) arr[j] += 1;
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+        } else {
+            for (int i = a; i >= b; i--) {
+                answer += i;
             }
         }
 
-        return arr;
+        return answer;
     }
 }
