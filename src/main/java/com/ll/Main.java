088d2d7b90a753c041a2a9c51210e1c1524cbc5e
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-18 14:34:18 +0900

    lv1. 나누어 떨어지는 숫자 배열

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,26 +1,31 @@
 package com.ll;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(5, 3));
+//        int[] arr = {5, 9, 7, 10}; // 5
+//        int[] arr = {3,2,6}; // 10
+        int[] arr = {2, 36, 1, 3}; // 1
+        System.out.println(Arrays.toString(s.solution(arr, 1)));
     }
 }
 
 class Solution {
-    public long solution(int a, int b) {
-        long answer = 0;
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
 
-        if (a < b) {
-            for (int i = a; i <= b; i++) {
-                answer += i;
-            }
-        } else {
-            for (int i = a; i >= b; i--) {
-                answer += i;
-            }
+        int[] filteredArr = Arrays.stream(arr)
+                .filter(a -> a % divisor == 0)
+                .toArray();
+
+        if (filteredArr.length == 0) {
+            answer = new int[1];
+            answer[0] = -1;
+            return answer;
         }
 
-        return answer;
+        return Arrays.stream(filteredArr).sorted().toArray();
     }
 }
