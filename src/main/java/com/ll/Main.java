9c726be9cc2caf4132fede1026450cd0ae29bcaf
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-20 17:59:28 +0900

    lv.0 문자열 묶기 - Map 사용법

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,23 +1,46 @@
 package com.ll;
 
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution("baconlettucetomato")));
+        String[] strArr = {"a", "bc", "d", "efg", "hi"};
+        System.out.println(s.solution(strArr));
     }
 }
 
 class Solution {
-    public String[] solution(String myStr) {
-        String[] splitStr = myStr.split("[abc]+");
-        String[] answer = Arrays.stream(splitStr).filter(str -> !Objects.equals(str, ""))
-                .toArray(String[]::new);
+    public int solution(String[] strArr) {
+        int[] strCount = new int[strArr.length];
 
-        if (answer.length == 0) return new String[]{"EMPTY"};
+        for (int i = 0; i < strCount.length; i++) {
+            strCount[i] = strArr[i].length();
+        }
 
-        return answer;
+        Map<Integer, Integer> myMap =new HashMap<>();
+
+        Arrays.stream(strCount).forEach(str -> {
+            // 값이 있으면
+            if (myMap.containsKey(str)) {
+                Integer count = myMap.get(str);
+                myMap.put(str, ++count);
+            } else { // 값이 없으면
+                myMap.put(str, 1);
+            }
+        });
+
+        // Map 반복문으로 순회, 큰 값 꺼내기
+        int max = 0;
+        for (Map.Entry<Integer, Integer> entry : myMap.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+            if (max < value) max = value;
+        }
+
+        return max;
     }
 }
