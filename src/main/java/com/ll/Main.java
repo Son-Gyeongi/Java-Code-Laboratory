474368378b6b7c8aeeacbadc1f1494ac3d7763ae
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-23 14:32:12 +0900

    lv1. 약수의 개수와 덧셈

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,27 +1,23 @@
 package com.ll;
 
-import java.util.Objects;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] array = {7, 77, 17};
-        System.out.println(s.solution(array));
+        System.out.println(s.solution(24, 27));
     }
 }
 
 class Solution {
-    public int solution(int[] array) {
+    public int solution(int left, int right) {
         int answer = 0;
 
-        for (int i = 0; i < array.length; i++) {
-            String[] splitStr = String.valueOf(array[i]).split("");
-
-            for (int j = 0; j < splitStr.length; j++) {
-                if (Objects.equals(splitStr[j], "7")) {
-                    answer++;
-                }
+        for (int i = left; i <= right; i++) {
+            // 약수가 홀수개
+            if (Math.pow((int) Math.sqrt(i), 2) == i) {
+                answer -= i;
+                continue;
             }
+            answer += i;
         }
 
         return answer;
