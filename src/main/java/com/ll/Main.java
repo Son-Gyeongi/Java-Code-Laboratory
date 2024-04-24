ec034e45c64925b776e1822c6b041becd0da0d3e
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-25 00:22:08 +0900

    lv1. 문자열 다루기 기본

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,27 +5,16 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = {1, 2, 3, 100, 99, 98};
-        System.out.println(s.solution(arr));
+        System.out.println(s.solution("1234"));
     }
 }
 
 class Solution {
-    public int solution(int[] arr) {
-        int count = 0;
-
-        while(true) {
-            int[] beforeArr = Arrays.copyOf(arr, arr.length);
-            for(int i=0;i<arr.length;i++) {
-                if(arr[i] % 2 ==0 && 50 <= arr[i]) arr[i] = arr[i]/2;
-                else if (arr[i] % 2 == 1 && arr[i]<50) arr[i] = (arr[i]*2)+1;
-            }
-            count++;
-
-            for(int i=0, j=0;i<arr.length;i++, j++) {
-                if(arr[i] != beforeArr[j]) break;
-                if(i == arr.length -1) return --count;
-            }
+    public boolean solution(String s) {
+        if (!(s.length() == 4 || s.length() == 6)) {
+            return false;
         }
+        String[] strArr = s.split("[0-9]");
+        return strArr.length == 0 ? true : false;
     }
 }
