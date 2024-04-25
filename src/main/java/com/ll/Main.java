56260b1c2227361b1f1f5308672629080e104033
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-25 11:28:43 +0900

    lv0. 커피 심부름

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,16 +5,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("1234"));
+        String[] order = {"cafelatte", "americanoice", "hotcafelatte", "anything"};
+        System.out.println(s.solution(order));
     }
 }
 
 class Solution {
-    public boolean solution(String s) {
-        if (!(s.length() == 4 || s.length() == 6)) {
-            return false;
-        }
-        String[] strArr = s.split("[0-9]");
-        return strArr.length == 0 ? true : false;
+    public int solution(String[] order) {
+        // 아메리카노 4,500원 / 카페라테 5,000원
+        // 차가운 지 따뜻한 지 구별 안해도 됨
+        // "anything"인 '아무거나'는 아메리카노
+
+        int americanoCount = (int) Arrays.stream(order)
+                .filter(o1 -> o1.contains("americano") || o1.contains("anything"))
+                .count();
+
+        int cafelatteCount = (int) Arrays.stream(order)
+                .filter(o2 -> o2.contains("cafelatte"))
+                .count();
+
+        return (americanoCount * 4500) + (cafelatteCount * 5000);
     }
 }
