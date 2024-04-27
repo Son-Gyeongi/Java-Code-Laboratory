2d4fd0b1e5a0dc2fbd74d1dc2bd33e7b19f758d8
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-27 18:30:39 +0900

    lv.1 최대공약수와 최소공배수

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,26 +1,33 @@
 package com.ll;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("1 2 Z 3"));
+        System.out.println(Arrays.toString(s.solution(3, 12)));
+//        System.out.println(Arrays.toString(s.solution(1_000_000, 999_999))); // 타입을 long으로
+//        System.out.println(Arrays.toString(s.solution(1_000_000, 1_000_000)));
     }
 }
 
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
+    public int[] solution(int n, int m) {
+        int max = 0; // 최대공약수 저장
+        int min; // 최소공배수 저장
 
-        String[] splitStr = s.split(" ");
+        // n, m 작은 거 구하기 어차피 작은거 보다 큰 공약수 나올 수 없음
+        int len = Math.min(n, m);
 
-        for(int i=0;i<splitStr.length;i++) {
-            if (Objects.equals(splitStr[i], "Z"))
-                splitStr[i] = String.valueOf(-Integer.parseInt(splitStr[i - 1]));
-            answer += Integer.parseInt(splitStr[i]);
+        for (int i = len; i >= 1; i--) {
+            if (n % i == 0 && m % i == 0) {
+                max = i;
+                break;
+            }
         }
 
-        return answer;
+        min = n * m / max;
+
+        return new int[]{max, min};
     }
 }
