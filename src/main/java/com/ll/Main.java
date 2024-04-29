3ae2be8b7fd81f688167978ff66d062650795f76
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-29 10:32:42 +0900

    lv1. 3진법 뒤집기 - Integer.parseInt(String s, int radix)

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,33 +1,21 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(3, 12)));
-//        System.out.println(Arrays.toString(s.solution(1_000_000, 999_999))); // 타입을 long으로
-//        System.out.println(Arrays.toString(s.solution(1_000_000, 1_000_000)));
+        System.out.println(s.solution(45));
     }
 }
 
 class Solution {
-    public int[] solution(int n, int m) {
-        int max = 0; // 최대공약수 저장
-        int min; // 최소공배수 저장
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
 
-        // n, m 작은 거 구하기 어차피 작은거 보다 큰 공약수 나올 수 없음
-        int len = Math.min(n, m);
-
-        for (int i = len; i >= 1; i--) {
-            if (n % i == 0 && m % i == 0) {
-                max = i;
-                break;
-            }
+        while (n > 0) {
+            sb.append(n % 3);
+            n /= 3;
         }
 
-        min = n * m / max;
-
-        return new int[]{max, min};
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
