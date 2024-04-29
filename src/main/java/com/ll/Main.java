2bb49b26564789512417c9cdb81392c7f659a08d
Son-Gyeongi <thsruddl77@gmail.com>
2024-04-29 11:55:18 +0900

    lv.0 소인수분해 - Set 은 중복 없고, 순서 없음

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,21 +1,33 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(45));
+        System.out.println(Arrays.toString(s.solution(420)));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        StringBuilder sb = new StringBuilder();
+    public int[] solution(int n) {
+        // Set은 중복, 순서 없음 - TreeSet은 오름차순 정렬해줌
+        Set<Integer> answer = new TreeSet<>();
 
-        while (n > 0) {
-            sb.append(n % 3);
-            n /= 3;
+        for (int i=2;i<=n;i++) {
+            while(n % i == 0) {
+                answer.add(i);
+                n /= i;
+            }
+
+            if (n/i == 1) {
+                answer.add(n);
+                break;
+            }
         }
 
-        return Integer.parseInt(sb.toString(), 3);
+        return answer.stream().mapToInt(a -> a).toArray();
     }
 }
