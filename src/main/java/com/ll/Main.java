2603624bada1a1585d1556782c34a7fae2593a03
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-11 23:37:46 +0900

    lv0. 공 던지기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,20 +5,45 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("onetwothreefourfivesixseveneightnine"));
+        int[] numbers = {1, 2, 3, 4};
+//        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(s.solution(numbers, 3));
     }
 }
 
 class Solution {
-    public long solution(String numbers) {
-        String[] numberArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    public int solution(int[] numbers, int k) {
+        int[] even = new int[numbers.length/2];
+        int[] odd = new int[numbers.length];
+        int j =0;
 
-        for (String s : numberArr) {
-            int index = Arrays.asList(numberArr).indexOf(s);
-            // replace() 사용할 때 따로 저장 해야 하는 구나
-            numbers = numbers.replace(s, String.valueOf(index));
+        // 1. 규칙 찾기
+        // 2. 찾은 규칙으로 배열 만들기
+        if (numbers.length % 2 == 0) {
+            // 배열이 짝수인 경우
+            for (int i=0;i<even.length;i++) {
+                even[i] = numbers[j];
+                j+=2;
+            }
+
+            System.out.println(Arrays.toString(even));
+            // 3. k-1 번째 값 반환
+            return even[(k - 1) % even.length];
+        } else {
+            // 배열이 홀수인 경우
+            for (int i=0;i<odd.length;i++) {
+                odd[i] = numbers[j];
+
+                if (j == numbers.length - 1) {
+                    j = 1;
+                    continue;
+                }
+                j+=2;
+            }
+
+            System.out.println(Arrays.toString(odd));
+            // 3. k-1 번째 값 반환
+            return odd[(k - 1) % odd.length];
         }
-
-        return Long.parseLong(numbers);
     }
 }
