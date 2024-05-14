b75a6518bc80914ca77459f05eca9b6d051d6a5f
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-14 11:08:14 +0900

    lv0. 문자열 여러 번 뒤집기 - 새로운 방법

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -9,30 +9,13 @@ public class Main {
 }
 
 class Solution {
-    public String solution(String my_string, int[][] queries) {
-        String[] strArr = my_string.split("");
-
-        for (int[] query : queries) {
-//            boolean queryOdd = (query[1] - query[0] + 1) % 2 == 1; // 홀수면 중간 숫자 있음
-            int changeIdx = (query[1] + query[0]) / 2;
-            int j = query[0];
-
-            for (int i=query[1];i> changeIdx;i--) {
-                String temp = strArr[i];
-                strArr[i] = strArr[j];
-                strArr[j] = temp;
-
-                j++;
-
-                // 중간값은 굳이 계산 안해도 되겠구나
-                /*if(queryOdd && (i + 1) == changeIdx) {
-                    strArr[i+1] = strArr[i+1];
-                }*/
-            }
+    public String solution(String myString, int[][] queries) {
+        for (int[] arr : queries) {
+            myString = myString.substring(0, arr[0])
+                    + new StringBuilder(myString.substring(arr[0], arr[1] + 1))
+                    .reverse().toString()
+                    + myString.substring(arr[1] + 1);
         }
-
-        // strArr => [p, r, o, g, r, a, m, m, e, r, s]
-//        return Arrays.stream(strArr).collect(Collectors.joining());
-        return String.join("", strArr);
+        return myString;
     }
 }
