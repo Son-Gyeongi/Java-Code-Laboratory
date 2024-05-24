0a08f7e2da771fb89dcf11d3f36d5a5482eeda20
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-24 21:28:02 +0900

    lv.0 대소문자 바꿔서 출력하기

--- src/main/java/com/ll/Solution1.java
+++ src/main/java/com/ll/Solution1.java
@@ -1,9 +1,26 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Solution1 {
     public static void main(String[] args) {
-        // !@#$%^&*(\'"<>?:;
-        // 특수문에서 역슬래쉬(\) 붙이기 - \ 와 ' 와 " 에 붙이기
-        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        String[] strArr = a.split("");
+        char[] charArr = a.toCharArray();
+
+        for (int i=0;i<charArr.length;i++) {
+            if ('a' <= charArr[i] && charArr[i] <= 'z') {
+                strArr[i] = strArr[i].toUpperCase();
+            } else {
+                strArr[i] = strArr[i].toLowerCase();
+            }
+        }
+
+        System.out.println(Arrays.stream(strArr).collect(Collectors.joining()));
+        System.out.println(String.join("", strArr));
     }
 }
