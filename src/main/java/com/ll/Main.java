7c91753f7995162e87d386260fc734d821d43261
Son-Gyeongi <thsruddl77@gmail.com>
2024-05-27 18:41:40 +0900

    lv0. 저주의 숫자 3

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,47 +1,31 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-//        int[] rank = {3, 7, 2, 5, 4, 6, 1};
-        int[] rank = {6, 1, 5, 2, 3, 4};
-//        boolean[] attendance = {false, true, true, true, true, false, false};
-        boolean[] attendance = {true, false, true, false, false, true};
-        System.out.println(s.solution(rank, attendance));
+        System.out.println(s.solution(73));
     }
 }
 
 class Solution {
-    public int solution(int[] rank, boolean[] attendance) {
+    public int solution(int n) {
         int answer = 0;
-        int[] finalRank = new int[rank.length];
 
-        for (int i=0;i<rank.length;i++) {
-            if (attendance[i]) finalRank[i] = rank[i];
-            else finalRank[i] = -1;
-        }
+        for (int i=1;i<=n;i++) {
 
-        System.out.println(Arrays.toString(finalRank));
+            answer++;
 
-        int count = 0;
-        for (int i=0;i<=100;i++) {
-            for (int j=0;j<finalRank.length;j++) {
-                if(i == finalRank[j] && count == 0) {
-                    answer += (10000 * j);
-                    count++;
-                } else if(i == finalRank[j] && count == 1) {
-                    answer += (100 * j);
-                    count++;
-                } else if(i == finalRank[j] && count == 2) {
-                    answer += j;
-                    count++;
-                    break;
-                }
+            while (answer %3 == 0 && answer > 0) answer++;
 
-                if (count == 3) break;
+            // 문자열로 3 포함하는지 보기
+            String str = String.valueOf(answer);
+            while(str.contains("3")) {
+                answer++;
+                str = String.valueOf(answer);
             }
+
+            while (answer %3 == 0 && answer > 0) answer++;
+            // System.out.printf("%d ==> %d\n", i, answer);
         }
 
         return answer;
