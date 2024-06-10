8c132ab4b2570a17fbdcb1da796826c7bb20b6b4
Son-Gyeongi <thsruddl77@gmail.com>
2024-06-11 06:19:01 +0900

    lv0. 한 번만 등장한 문자

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,22 +1,48 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("Progra21Sremm3", 6, 12));
+        System.out.println(s.solution("abcabcadc"));
+        System.out.println(s.solution("abdc"));
+        System.out.println(s.solution("hello"));
     }
 }
 
 class Solution {
-    public String solution(String my_string, int s, int e) {
-        String a = my_string.substring(0, s); // 0부터 s-1까지
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
 
-        String b = my_string.substring(s, e + 1); // s부터 e까지
-        StringBuilder sb = new StringBuilder(b);
-        String reverse = sb.reverse().toString();
+        Map<String, Integer> map = new HashMap<>();
 
-        String c = my_string.substring(e+1); // e+1 부터 끝까지
+        for (String string : str) {
+            // 값이 있으면
+            if (map.containsKey(string)) {
+                Integer count = map.get(string);
+                map.put(string, ++count);
+            } else {
+                // 값이 없으면
+                map.put(string, 1);
+            }
+        }
 
-        return a + reverse + c;
+        for (String string : str) {
+            Integer count = map.get(string);
+
+            if (count == 1) {
+                answer += string;
+            }
+        }
+
+        // answer 영어 오름차순으로 정렬
+        String[] strSplit = answer.split("");
+        Arrays.sort(strSplit);
+
+        return String.join("", strSplit);
     }
 }
