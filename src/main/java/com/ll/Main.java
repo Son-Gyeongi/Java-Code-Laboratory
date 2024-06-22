67c331f71442d4a4e253a3032f1fb5f6203fd242
Son-Gyeongi <thsruddl77@gmail.com>
2024-06-22 16:46:40 +0900

    lv.0 안전지대

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,30 +5,65 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] picture = {".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."};
-        System.out.println(Arrays.toString(s.solution(picture, 2)));
+        int[][] board = {{1, 1, 1, 1, 1, 1},
+                            {1, 1, 1, 1, 1, 1},
+                            {1, 1, 1, 1, 1, 1},
+                            {1, 1, 1, 1, 1, 1},
+                            {1, 1, 1, 1, 1, 1},
+                            {1, 1, 1, 1, 1, 1}};
+        System.out.println(s.solution(board));
     }
 }
 
 class Solution {
-    public String[] solution(String[] picture, int k) {
-        String[] answer = new String[picture.length*k];
+    public int solution(int[][] board) {
+        int answer = 0;
+        int[][] temp = new int[board.length][board[0].length];
 
-        int x=0;
-        int increase = k;
-        for (int i=0;i<picture.length;i++) {
-            System.out.println("i = "+i);
-            String temp = "";
-            String[] picArr = picture[i].split("");
-
-            for (int j=0;j<picArr.length;j++) {
-                temp += picArr[j].repeat(k);
+        // board에 행과 열 추가해서 감싸기
+        int[][] boardCopy = new int[board.length+2][board[0].length+2];
+        for (int i=1;i<boardCopy.length-1;i++) {
+            for (int j=1;j<boardCopy[i].length-1;j++) {
+                boardCopy[i][j] = board[i-1][j-1];
             }
+        }
+        // System.out.println(Arrays.deepToString(boardCopy));
 
-            for (;x<increase;x++) {
-                answer[x] = temp;
+
+        for (int i=1;i<boardCopy.length-1;i++) {
+            for (int j=1;j<boardCopy[i].length-1;j++) {
+                if (boardCopy[i][j] == 1) {
+                    temp[i-1][j-1] = 1;
+                } else {
+                    // i-1 행 검사
+                    if (boardCopy[i-1][j-1] == 1 || boardCopy[i-1][j] == 1 || boardCopy[i-1][j+1] == 1) {
+                        temp[i-1][j-1] += 1;
+                    }
+
+                    // i+1 행 검사
+                    if (boardCopy[i+1][j-1] == 1 || boardCopy[i+1][j] == 1 || boardCopy[i+1][j+1] == 1) {
+                        temp[i-1][j-1] += 1;
+                    }
+
+                    // j-1 열 검사
+                    if (boardCopy[i-1][j-1] == 1 || boardCopy[i][j-1] == 1 || boardCopy[i+1][j-1] == 1) {
+                        temp[i-1][j-1] += 1;
+                    }
+
+                    // j+1 열 검사
+                    if (boardCopy[i-1][j+1] == 1 || boardCopy[i][j+1] == 1 || boardCopy[i+1][j+1] == 1) {
+                        temp[i-1][j-1] += 1;
+                    }
+                }
             }
-            increase+=k;
+        }
+
+         System.out.println(Arrays.deepToString(temp));
+        // temp에 0의 개수 세기
+        for (int i=1;i<temp.length;i++) {
+            for (int j=0;j<temp[i].length;j++) {
+                if (temp[i][j] == 0) answer +=1;
+            }
         }
 
         return answer;
