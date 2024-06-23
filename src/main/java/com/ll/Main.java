1be228732754149f3538c391413ed3d3114ccbea
Son-Gyeongi <thsruddl77@gmail.com>
2024-06-23 22:53:29 +0900

    lv.0 평행

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,66 +5,33 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] board = {{1, 1, 1, 1, 1, 1},
-                            {1, 1, 1, 1, 1, 1},
-                            {1, 1, 1, 1, 1, 1},
-                            {1, 1, 1, 1, 1, 1},
-                            {1, 1, 1, 1, 1, 1},
-                            {1, 1, 1, 1, 1, 1}};
-        System.out.println(s.solution(board));
+        int[][] dots = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+        System.out.println(s.solution(dots));
     }
 }
 
 class Solution {
-    public int solution(int[][] board) {
+    public int solution(int[][] dots) {
         int answer = 0;
-        int[][] temp = new int[board.length][board[0].length];
 
-        // board에 행과 열 추가해서 감싸기
-        int[][] boardCopy = new int[board.length+2][board[0].length+2];
-        for (int i=1;i<boardCopy.length-1;i++) {
-            for (int j=1;j<boardCopy[i].length-1;j++) {
-                boardCopy[i][j] = board[i-1][j-1];
-            }
-        }
-        // System.out.println(Arrays.deepToString(boardCopy));
+        // 0,1,2,3 은 dots의 인덱스를 말함
+        // 좌표 0,1 와 2,3 일 경우
+        if ((dots[1][0] - dots[0][0] == dots[3][0] - dots[2][0])
+                && (dots[1][1] - dots[0][1] == dots[3][1] - dots[2][1])) return 1;
+        else if (((double)(dots[1][1] - dots[0][1])/(dots[1][0] - dots[0][0]))
+                == ((double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]))) return 1;
 
+        // 좌표 0,2 와 1,3 일 경우
+        if ((dots[2][0] - dots[0][0] == dots[3][0] - dots[1][0])
+                && (dots[2][1] - dots[0][1] == dots[3][1] - dots[1][1])) return 1;
+        else if (((double)(dots[2][1] - dots[0][1])/(dots[2][0] - dots[0][0]))
+                == ((double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]))) return 1;
 
-        for (int i=1;i<boardCopy.length-1;i++) {
-            for (int j=1;j<boardCopy[i].length-1;j++) {
-                if (boardCopy[i][j] == 1) {
-                    temp[i-1][j-1] = 1;
-                } else {
-                    // i-1 행 검사
-                    if (boardCopy[i-1][j-1] == 1 || boardCopy[i-1][j] == 1 || boardCopy[i-1][j+1] == 1) {
-                        temp[i-1][j-1] += 1;
-                    }
-
-                    // i+1 행 검사
-                    if (boardCopy[i+1][j-1] == 1 || boardCopy[i+1][j] == 1 || boardCopy[i+1][j+1] == 1) {
-                        temp[i-1][j-1] += 1;
-                    }
-
-                    // j-1 열 검사
-                    if (boardCopy[i-1][j-1] == 1 || boardCopy[i][j-1] == 1 || boardCopy[i+1][j-1] == 1) {
-                        temp[i-1][j-1] += 1;
-                    }
-
-                    // j+1 열 검사
-                    if (boardCopy[i-1][j+1] == 1 || boardCopy[i][j+1] == 1 || boardCopy[i+1][j+1] == 1) {
-                        temp[i-1][j-1] += 1;
-                    }
-                }
-            }
-        }
-
-         System.out.println(Arrays.deepToString(temp));
-        // temp에 0의 개수 세기
-        for (int i=1;i<temp.length;i++) {
-            for (int j=0;j<temp[i].length;j++) {
-                if (temp[i][j] == 0) answer +=1;
-            }
-        }
+        // 좌표 0,3 와 1,2 일 경우
+        if ((dots[3][0] - dots[0][0] == dots[2][0] - dots[1][0])
+                && (dots[3][1] - dots[0][1] == dots[2][1] - dots[1][1])) return 1;
+        else if (((double)(dots[3][1] - dots[0][1])/(dots[3][0] - dots[0][0]))
+                == ((double)(dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]))) return 1;
 
         return answer;
     }
