795a74357ca2b0b161de37fa162306f9fd48292a
Son-Gyeongi <thsruddl77@gmail.com>
2024-07-07 19:36:31 +0900

    lv.1 카드 뭉치 - Queue 사용

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,38 +5,43 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-//        String[] strings = {"abce", "abcd", "cdx"}; // 2
-//        String[] strings = {"sun", "bed", "car"}; // 1
-        String[] strings = {"ae", "be", "ce", "ae"}; // 1
-        System.out.println(Arrays.toString(s.solution(strings, 1)));
+        String[] cards1 = {"i", "drink", "water"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+        System.out.println(s.solution(cards1, cards2, goal));
     }
 }
 
 class Solution {
-    public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-        int index = 0;
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        /*
+        Queue<String> queue1 = new LinkedList<>();
+        for (int i=0;i<cards1.length;i++) {
+            queue1.add(cards1[i]);
+        }
+        */
+        Queue<String> queue1 = new LinkedList<>(Arrays.asList(cards1));
 
-        // 1. a-z까지 반복문 돌리기
-        for (char start='a';start<='z';start++) {
-            // 2. strings 배열에서 각 요소들 중에 n인덱스랑 맞는 a-z 찾고 stringList에 넣기
-            List<String> tempList = new ArrayList<>();
-            for (String string : strings) {
-                if (start == string.charAt(n)) { // 3. 같은 값일 경우 비교 후에 넣기
-                    tempList.add(string);
-                }
-            }
+        /*
+        Queue<String> queue2 = new LinkedList<>();
+        for (int i=0;i<cards2.length;i++) {
+            queue2.add(cards2[i]);
+        }
+        */
+        Queue<String> queue2 = new LinkedList<>(Arrays.asList(cards2));
 
-            // tempList 정렬
-//            List<String> sortedList = tempList.stream().sorted().toList(); // 자바 16에서 추가된 것, 오름차순
-//            Collections.sort(tempList); // 오름차순
-            tempList.sort(Comparator.naturalOrder()); // 자바 8에서 추가된 것, 오름차순
-            // amswer 에 값 넣기
-            for (int i=0;i<tempList.size();i++) {
-                answer[index++] = tempList.get(i);
+        for (String s : goal) {
+            String str1 = queue1.isEmpty() ? "" : queue1.peek();
+            String str2 = queue2.isEmpty() ? "" : queue2.peek();
+            if (str1.equals(s)) {
+                queue1.poll();
+            } else if (str2.equals(s)) {
+                queue2.poll();
+            } else {
+                return "No";
             }
         }
 
-        return answer;
+        return "Yes";
     }
 }
