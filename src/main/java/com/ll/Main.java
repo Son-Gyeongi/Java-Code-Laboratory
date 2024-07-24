2f7206d381386b46290105128dd9fe5d267ccb14
Son-Gyeongi <thsruddl77@gmail.com>
2024-07-24 20:00:44 +0900

    lv.1 체육복

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,48 +1,61 @@
 package com.ll;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[] keymap = {"AA"};
-        String[] targets = {"B"};
-        System.out.println(Arrays.toString(s.solution(keymap, targets)));
+//        int[] lost = {2, 4};
+        int[] lost = {28, 30};
+//        int[] reserve = {1, 3, 5};
+        int[] reserve = {27, 29};
+        System.out.println(s.solution(30, lost, reserve));
     }
 }
 
 class Solution {
-    public int[] solution(String[] keymap, String[] targets) {
-        int[] answer = new int[targets.length];
-        Map<String, Integer> map = new HashMap<>();
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
 
-        // 1. Map 에 keymap 각 요소 누르는 값 넣기
-        for (int i=0;i<keymap.length;i++) {
-            String[] key = keymap[i].split("");
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
-            for (int j=0;j<key.length;j++) {
-                int value = map.getOrDefault(key[j], j+1);
+        // 1. 모든 n 학생 수를 Map에 넣기
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            map.put(i, 1);
+        }
 
-                map.put(key[j], value > j+1 ? j+1 : value);
+        // 2. lost 학생들은 0 값을 넣기
+        for (int i=0;i<lost.length;i++) {
+            map.put(lost[i], 0);
+        }
+
+        // 3. reserve를 확인해서 lost 학생들에게 옷을 빌려줄 수 있는지 확인
+        // 자기자신이 체육복 안 가져왔는지 확인
+        for (int i=0;i<reserve.length;i++) {
+            for (int j=0;j<lost.length;j++) {
+                if (reserve[i] == lost[j]) {
+                    map.put(lost[j], 1);
+                    reserve[i] = -1;
+                }
             }
         }
 
-        // 2. map에 맞게 targets 계산하기
-        for (int i=0;i<targets.length;i++) {
-            String[] target = targets[i].split("");
-
-            for (int j=0;j<target.length;j++) {
-                if (map.get(target[j]) == null) {
-                    answer[i] = -1;
-                    break;
+        // 다른 사람 체육복 빌리기
+        for (int i=0;i<reserve.length;i++) {
+            for (int j=0;j<lost.length;j++) {
+                if (reserve[i] == (lost[j] - 1) || reserve[i] == (lost[j] + 1)) {
+                    map.put(lost[j], 1);
+                    reserve[i] = -1; // 여벌 옷 빌려준 사람 제외
+                    lost[j] = -5; // 여벌 옷 빌린 사람 제외
                 }
-
-                int value = map.get(target[j]);
-
-                answer[i] += value;
             }
+        }
+
+        // 4. Map을 확인해서 1인 학생의 수 구하기 (체육복이 있는 학생 수 세기)
+        for (int i=1;i<=map.size();i++) {
+            if (map.get(i) == 1) answer++;
         }
 
         return answer;
