ae02e7c75f432d46ff30ff7a60afa37e61275434
Son-Gyeongi <thsruddl77@gmail.com>
2024-08-14 23:55:28 +0900

    lv1. 가장 많이 받은 선물 - 진행중...

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -7,6 +7,10 @@ public class Main {
         Solution s = new Solution();
         String[] friends = {"muzi", "ryan", "frodo", "neo"};
         String[] gifts = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
+//        String[] friends = {"joy", "brad", "alessandro", "conan", "david"};
+//        String[] gifts = {"alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"};
+//        String[] friends = {"a", "b", "c"};
+//        String[] gifts = {"a b", "b a", "c a", "a c", "a c", "c a"};
         System.out.println(s.solution(friends, gifts));
     }
 }
@@ -14,6 +18,7 @@ public class Main {
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
+        Map<String, Integer> nextMonthGifts = new HashMap<>();
 
         // 선물 준 사람과 그에 따른 선물 받은 사람 리스트 저장
         Map<String, Map<String, Integer>> giveGiftList = new HashMap<>();
@@ -30,9 +35,9 @@ class Solution {
             giveGiftList.get(giveFriend).put(takeFriend,
                     giveGiftList.get(giveFriend).getOrDefault(takeFriend, 0) + 1);
         }
-        System.out.println(giveFriends);
-        System.out.println(takeFriends);
-        System.out.println(giveGiftList);
+        System.out.println("giveFriends = " + giveFriends);
+        System.out.println("takeFriends = " + takeFriends);
+        System.out.println("giveGiftList = " + giveGiftList);
 
         // 선물 지수 저장
         Map<String, Integer> giftPoint = new HashMap<>();
@@ -42,23 +47,43 @@ class Solution {
 
             giftPoint.put(friends[i], giveCount-takeCount);
         }
-        System.out.println(giftPoint);
+        System.out.println("giftPoint = " + giftPoint);
 
 
-        for (int i=0;i<friends.length-1;i++) {
-            String A = friends[i];
 
-            for (int j=0;j<friends.length-1;j++) {
-                String B = friends[j];
+        System.out.println("nextMonthGifts = " + nextMonthGifts);
 
-                // 서로에게 한명이라도 선물을 준 적 있는지 확인
-                if (giveGiftList.contains(A).contains(B) ||
-                        giveGiftList.contains(B).contains(A)) {
-                    System.out.println("dfdf");
-                }
-            }
+        // nextMonthGifts에서 제일 큰 숫자 찾아서 리턴
+        for (String key : nextMonthGifts.keySet()) {
+            answer = Math.max(nextMonthGifts.get(key), answer);
         }
 
         return answer;
+    }
+
+    // 많이 선물한 사람이 다음달에 선물 한 개 더 받음
+    public void gaveMoreGifts(String person, Map<String, Integer> nextMonthGifts) {
+        System.out.println("gaveMoreGifts = "+person);
+        nextMonthGifts.put(person, nextMonthGifts.getOrDefault(person, 0) + 1);
+    }
+    public void gaveMoreGifts(Map<String, Map<String, Integer>> giveGiftList, String A, String B, Map<String, Integer> nextMonthGifts) {
+        // 선물 준 개수
+        int giveA = giveGiftList.containsKey(A) ? giveGiftList.get(A).getOrDefault(B, 0) : 0;
+        int giveB = giveGiftList.containsKey(B) ? giveGiftList.get(B).getOrDefault(A, 0) : 0;
+
+        if (giveA > giveB) nextMonthGifts.put(A, nextMonthGifts.getOrDefault(A, 0) + 1);
+    }
+
+    // 선물 지수 계산
+    public void giftPointCal(Map<String, Integer> giftPoint, String A, String B, Map<String, Integer> nextMonthGifts) {
+        System.out.println("giftPointCal = "+A);
+        System.out.println("giftPointCal = "+B);
+
+        // 선물 지수
+        int giftPointA = giftPoint.get(A);
+        int giftPointB = giftPoint.get(B);
+
+        if (giftPointA > giftPointB) nextMonthGifts.put(A, nextMonthGifts.getOrDefault(A, 0) + 1);
+        else if (giftPointA < giftPointB) nextMonthGifts.put(B, nextMonthGifts.getOrDefault(B, 0) + 1);
     }
 }
