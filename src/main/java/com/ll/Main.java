b005db754e55a64f534057286b573fc1ba3709a4
Son-Gyeongi <thsruddl77@gmail.com>
2024-08-29 23:54:33 +0900

    lv.2 구명보트

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,40 +5,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int []A = {1, 4, 2};
-        int []B = {5, 4, 4};
-        System.out.println(s.solution(A, B));
+        int[] people = {70, 50, 80, 50};
+//        int[] people = {70, 80, 50};
+        System.out.println(s.solution(people, 100));
     }
 }
 
-class Solution
-{
-    public int solution(int []A, int []B)
-    {
+class Solution {
+    public int solution(int[] people, int limit) {
         int answer = 0;
+        Arrays.sort(people);
+        int i = 0, j = people.length -1;
 
-        // 1. A 배열을 내림차순으로 저장하기
-        Integer[] a = new Integer[A.length];
-
-        for (int i=0;i<A.length;i++) {
-            a[i] = A[i];
-        }
-        Arrays.sort(a, Collections.reverseOrder());
-
-        // 1-1. B 배열을 오름차순으로 저장하기
-        Integer[] b = new Integer[B.length];
-
-        for (int i=0;i<B.length;i++) {
-            b[i] = B[i];
-        }
-        Arrays.sort(b);
-
-        // 2. A 각 요소와 B 모든 요소를 곱해서 작은 값을 answer에 더하기
-        for (int i=0;i<a.length;i++) {
-            int mulA = a[i];
-            int mulB = b[i];
-
-            answer += (mulA * mulB);
+        while (i  <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            answer++;
         }
 
         return answer;
