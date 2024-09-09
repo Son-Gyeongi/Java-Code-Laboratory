b535a07e23c9ac01e36a2d78404b2ff10a106a95
Son-Gyeongi <thsruddl77@gmail.com>
2024-09-09 20:49:42 +0900

    lv.2 n^2 배열 자르기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,24 +5,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] people = {70, 50, 80, 50};
-//        int[] people = {70, 80, 50};
-        System.out.println(s.solution(people, 100));
+//        System.out.println(Arrays.toString(s.solution(4, 7, 14)));
+        System.out.println(Arrays.toString(s.solution(10000000, 99999999999L, 99999999999L)));
     }
 }
 
 class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
-        int i = 0, j = people.length -1;
+    public int[] solution(int n, long left, long right) {
+        int length = (int) (right - left);
+        long[] procedure = new long[length+1];
+        int[] answer = new int[length+1];
 
-        while (i  <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++;
+        int answerIdx = 0;
+        for (long i = left;i<=right;i++) {
+            long mok = i / n; // 몫
+            long nam = i % n; // 나머지
+
+            if (mok < nam) {
+                procedure[answerIdx] = nam + 1;
+            } else {
+                procedure[answerIdx] = mok + 1;
             }
-            j--;
-            answer++;
+
+            answerIdx++;
+        }
+
+        // 타입을 long 에서 int 로 변경
+        for (int i=0;i<procedure.length;i++) {
+            answer[i] = (int) procedure[i];
         }
 
         return answer;
