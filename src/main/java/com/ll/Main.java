a83873186cb4c3a9e1ca8ec37af48ae6e418fcaf
Son-Gyeongi <thsruddl77@gmail.com>
2024-09-10 23:55:34 +0900

    lv.2 행렬의 곱셈 - 진행중

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,34 +5,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-//        System.out.println(Arrays.toString(s.solution(4, 7, 14)));
-        System.out.println(Arrays.toString(s.solution(10000000, 99999999999L, 99999999999L)));
+        int[][] arr1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] arr2 = {{1, 4}, {2, 5}, {3, 6}};
+        System.out.println(Arrays.deepToString(s.solution(arr1, arr2)));
     }
 }
 
 class Solution {
-    public int[] solution(int n, long left, long right) {
-        int length = (int) (right - left);
-        long[] procedure = new long[length+1];
-        int[] answer = new int[length+1];
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length]; // arr1의 행 길이, arr2의 열 길이
 
-        int answerIdx = 0;
-        for (long i = left;i<=right;i++) {
-            long mok = i / n; // 몫
-            long nam = i % n; // 나머지
+        for (int x=0;x<arr1.length;x++) { // arr1의 행
+            int arr1Point = 0;
 
-            if (mok < nam) {
-                procedure[answerIdx] = nam + 1;
-            } else {
-                procedure[answerIdx] = mok + 1;
+            for (int i=0;i<arr2.length;i++) { // arr2의 열
+                int j = 0; // arr2의 행
+
+                for (int y=0;y<arr1[0].length;y++) { // arr1의 열
+                    if (j >= arr2.length || i >= arr2[0].length) break;
+                    arr1Point = arr1[x][y];
+                    answer[x][i] += (arr1Point * arr2[j][i]);
+                    System.out.println("answer["+x+"]["+i+"] += ("+arr1Point+" * "+"arr2["+j+"]["+i+"])");
+                    j++;
+                }
             }
-
-            answerIdx++;
-        }
-
-        // 타입을 long 에서 int 로 변경
-        for (int i=0;i<procedure.length;i++) {
-            answer[i] = (int) procedure[i];
+            System.out.println();
         }
 
         return answer;
