3bc6e2f2c507ae471b0934e6ad3ffb6bdd26bfbe
Son-Gyeongi <thsruddl77@gmail.com>
2024-09-15 23:39:25 +0900

    lv.2 의상 - 진행중

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,30 +5,65 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[][] arr1 = {{1, 2, 3}, {4, 5, 6}};
-        int[][] arr2 = {{1, 4}, {2, 5}, {3, 6}};
-        System.out.println(Arrays.deepToString(s.solution(arr1, arr2)));
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(s.solution(clothes));
     }
 }
 
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length]; // arr1의 행 길이, arr2의 열 길이
+    public int solution(String[][] clothes) {
+        int answer = clothes.length; // 일단 기본 옷 1개씩 (중복되지 않는 의상 종류 개수 더하기)
 
-        for (int x=0;x<arr1.length;x++) { // arr1의 행
-            int arr1Point = 0;
+        System.out.println(answer);
 
-            for (int i=0;i<arr2[0].length;i++) { // arr2의 열
-                int j = 0; // arr2의 행
+        // 각 의상 종류에 맞는 의상 이름 저장하기
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i=0;i<clothes.length;i++) {
+            map.putIfAbsent(clothes[i][1], new ArrayList<>());
 
-                for (int y=0;y<arr1[0].length;y++) { // arr1의 열
-                    if (i >= arr2[0].length) break;
-                    arr1Point = arr1[x][y];
-                    answer[x][i] += (arr1Point * arr2[j][i]);
-                    j++;
-                }
+            map.get(clothes[i][1]).add(clothes[i][0]);
+        }
+
+        System.out.println(map);
+
+        // 그리고 조합 여러개 (의상 종류가 2개 이상일 때)
+        int[] array = new int[map.size()];
+        int idx = 0;
+        if (1 < map.size()) {
+            for (String key : map.keySet()) {
+                array[idx++] = map.get(key).size(); // 의상 종류에 옷 개수
             }
         }
+
+        // 각 의상 종류의 옷 개수를 배열에 담았다.
+        int totalSum = 0;
+        for (int i=0;i<array.length-1;i++) {
+            int value1 = array[i];
+            int sum = 0;
+
+            for (int j=i+1;j<array.length;j++) {
+                int value2 = array[j];
+
+                sum += (value1*value2);
+            }
+
+            totalSum += sum;
+        }
+        System.out.println("totalSum = "+totalSum);
+
+        // map 사이즈가 3이상인 경우 - 옷을 모두 입는 경우
+        if (map.size() > 2) {
+            int mul = 1;
+
+            for (int i=0;i<array.length;i++) {
+                mul *= array[i];
+            }
+
+            totalSum += mul;
+            System.out.println("mul = "+mul);
+        }
+
+        answer += totalSum;
 
         return answer;
     }
